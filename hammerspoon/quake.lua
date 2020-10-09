4173002c79a363fd38f60ea626772e305050fb14
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-10-09 18:20:30 +0200

    kitty quake 0.95 opacity

--- hammerspoon/quake.lua
+++ hammerspoon/quake.lua
@@ -39,7 +39,7 @@ module.toggle = function()
         print("kitty: launch")
 
         os.execute(
-            "/usr/local/bin/kitty -d ~ --title scratchpad -1 --instance-group scratchpad -o background_opacity=0.9 -o macos_hide_from_tasks=yes -o macos_quit_when_last_window_closed=yes &")
+            "/usr/local/bin/kitty -d ~ --title scratchpad -1 --instance-group scratchpad -o background_opacity=0.95 -o macos_hide_from_tasks=yes -o macos_quit_when_last_window_closed=yes &")
     end
 end
 
