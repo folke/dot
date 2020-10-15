bee5904aeb8542a43074907e4a41a8c03e719388
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-10-15 09:00:02 +0200

    Lispy!!

--- config/doom/init.el
+++ config/doom/init.el
@@ -58,7 +58,7 @@
        fold              ; (nigh) universal code folding
        (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
-       ;;lispy             ; vim for lisp, for people who don't like vim
+       lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
