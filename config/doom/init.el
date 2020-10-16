b702d19900136ee06e2254717939095d5b237c3f
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-10-16 20:44:51 +0200

    disable indent guides

--- config/doom/init.el
+++ config/doom/init.el
@@ -35,7 +35,7 @@
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       indent-guides     ; highlighted indent columns
+       ;;indent-guides     ; highlighted indent columns
        (ligatures +extra)         ; ligatures and symbols to make your code pretty again
        ;;minimap         ; show a map of the code on the side
        modeline         ; snazzy, Atom-inspired modeline, plus API
