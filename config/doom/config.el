6e24199c513cd13b836a5cb6e4e8bf3a08486550
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-10-16 14:14:39 +0200

    default doom theme

--- config/doom/config.el
+++ config/doom/config.el
@@ -79,7 +79,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-moonlight)
+;;(setq doom-theme 'doom-moonlight)
 ;;(setq doom-theme 'doom-palenight)
 
 (after! centaur-tabs
