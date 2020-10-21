2b7fe98b092110f57f1762b2bc4dd60503d8ce94
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-10-21 16:16:09 +0200

    moonlight theme

--- config/doom/config.el
+++ config/doom/config.el
@@ -91,7 +91,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-moonlight)
+(setq doom-theme 'doom-moonlight)
 ;;(setq doom-theme 'doom-palenight)
 ;;(setq doom-theme 'doom-dracula)
 
