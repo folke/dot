2ba642fab1b6287a9448b1c35ad5cc92d1ad4b99
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-10-26 12:29:35 +0100

    removed thefuck

--- config/fish/functions/fuck.fish
+++
@@ -1,4 +0,0 @@
-function fuck -d "Correct your previous console command"
-  __tf_alias
-  fish -c __tf_updt 2>&1 > /dev/null &
-end
--- config/fish/functions/thefuck-command-line.fish
+++
@@ -1,5 +0,0 @@
-function thefuck-command-line
-  env THEFUCK_REQUIRE_CONFIRMATION=0 thefuck $history[1] 2> /dev/null | read fuck
-  [ -z $fuck ]; and return
-  commandline -r $fuck
-end
