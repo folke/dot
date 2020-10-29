c86f263898d52790213889c275e04f56adb1aafd
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-10-29 10:48:43 +0100

    fixed multiline history with fzf

--- config/fish/functions/fzf_history.fish
+++ config/fish/functions/fzf_history.fish
@@ -1,15 +1,17 @@
 # originally implemented and transposed from https://github.com/patrickf3139/dotfiles/pull/11
 function fzf_history --description "Search command history using fzf. Replace the commandline with the selected command."
-
-    set | rg "^fish_color" | sed "s/^/set -x /" | source
     # history merge incorporates history changes from other fish sessions
     history merge
-    set cmd (
-        history | fish_indent --ansi | fzf --tiebreak=index --ansi --query=(commandline)
-    )
+    history --null \
+        | string replace -ar "\x0" ";__delim__;" \
+        | fish_indent_ansi -i \
+        | gsed "s/__delim__/\\x0/g" \
+        | fzf --tiebreak=index --read0 --ansi --query=(commandline) \
+        | read -lz cmd
 
     if test $status -eq 0
-        commandline --replace $cmd
+        # trim any surrounding white space
+        commandline --replace (echo $cmd | gsed -zr "s/^\s+|\s+\$//g")
     end
 
     commandline --function repaint
