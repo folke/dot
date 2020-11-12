d6464005a038153eaa9d2ddda6ee848e3afc03f6
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-11-12 18:42:20 +0100

    fixed path priority between fnm and pnpm

--- config/fish/conf.d/fnm.fish
+++ config/fish/conf.d/fnm.fish
@@ -1,13 +1,10 @@
 
 set -p PATH ~/.fnm/aliases/default/bin
+set -p PATH ~/.local/bin/pnpm
 
 function _fnm_autoload_hook --on-variable PWD --description 'Change Node version on directory change'
     status --is-command-substitution; and return
-    if test -f .node-version
-        echo "fnm: Found .node-version"
-        fnm use
-    else if test -f .nvmrc
-        echo "fnm: Found .nvmrc"
+    if test -f .node-version -o -f .nvmrc
         fnm use
     end
 end
--- config/fish/functions/fnm.fish
+++ config/fish/functions/fnm.fish
@@ -2,7 +2,8 @@
 
 function fnm --wrap='fnm'
     if test -z $fnm_initialized
-        command fnm env --shell=fish --use-on-cd | source
+        command fnm env --shell=fish | source
+        set -p PATH ~/.local/bin/pnpm
         set -g fnm_initialized 1
     end
     command fnm $argv
