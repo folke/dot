6ae1f797f8726cd3d466b33b909d50789e12afe4
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-11-23 08:51:05 +0100

    fixed border on non standard windows

--- hammerspoon/running.lua
+++ hammerspoon/running.lua
@@ -28,7 +28,9 @@ module.getWindowsPerSpace = function()
             if ax:isValid() then
                 local win = ax:asHSWindow()
                 for _, space in pairs(win:spaces()) do
-                    if ret[space] == nil then ret[space] = {} end
+                    if ret[space] == nil then
+                        ret[space] = {}
+                    end
                     ret[space][win:id()] = win
                 end
             end
@@ -66,7 +68,9 @@ end
 module._addAppWindow = function(app, ax)
     if ax ~= nil and ax.AXSubrole == "AXStandardWindow" then
         local pid = app:pid()
-        if module.windows[pid] == nil then module.windows[pid] = {} end
+        if module.windows[pid] == nil then
+            module.windows[pid] = {}
+        end
         local win = ax:asHSWindow()
         if not module.windows[pid][win:id()] then
             module.windows[pid][win:id()] = ax
@@ -76,7 +80,9 @@ module._addAppWindow = function(app, ax)
 end
 
 module._updateAppWindows = function(app, ax)
-    if module.windows[app:pid()] == nil then module.windows[app:pid()] = {} end
+    if module.windows[app:pid()] == nil then
+        module.windows[app:pid()] = {}
+    end
     for _, child in ipairs(ax.AXChildren) do
         if child:matchesCriteria("AXWindow") then
             module._addAppWindow(app, child)
@@ -96,7 +102,9 @@ end
 
 module.triggerChange = function(app, win, event)
     local winTitle = ""
-    if win then winTitle = win:title() end
+    if win then
+        winTitle = win:title()
+    end
     print(">> " .. event .. ":" .. app:name() .. " -- " .. winTitle)
     for _, fn in ipairs(module._listeners) do
         fn(app, win, event)
@@ -108,15 +116,19 @@ module._listeners = {}
 module.onChange = function(fn)
     table.insert(module._listeners, fn)
     local win = hs.window.focusedWindow()
-    if win ~= nil then fn(win:application(), win, module.events.focused) end
+    if win ~= nil then
+        fn(win:application(), win, module.events.focused)
+    end
 end
 
 module._watchApp = function(app)
-    if module.apps[app:pid()] ~= nil then return end
+    if module.apps[app:pid()] ~= nil then
+        return
+    end
     local ax = hs.axuielement.applicationElement(app)
     -- when the app just launched, we might have to wait for
     -- the next event to setup the observers
-    if ax:isValid() and app:kind() > 0 then
+    if ax:isValid() then
         print("## watching: " .. app:name())
         module.apps[app:pid()] = app
         module._updateAppWindows(app, ax)
@@ -129,24 +141,30 @@ module._watchApp = function(app)
         w:addWatcher(ax, "AXUIElementDestroyed")
 
         -- w:addWatcher(ax, "AXUIElementDestroyed")
-        w:callback(function(_, axel, notif, notifData)
-            if notif == "AXUIElementDestroyed" then
-                if not app:focusedWindow() then
-                    module._updateAppWindows(app, ax)
+        w:callback(
+            function(_, axel, notif, notifData)
+                print(hs.inspect(notif))
+                if notif == "AXUIElementDestroyed" then
+                    if not app:focusedWindow() then
+                        module._updateAppWindows(app, ax)
+                    end
+                    return
                 end
-                return
-            end
 
-            if not axel:matchesCriteria("AXWindow") then return end
-
-            local win = axel:asHSWindow()
-            if notif == "AXFocusedWindowChanged" then
-                module._updateAppWindows(app, ax)
-                module.triggerChange(app, win, module.events.focused)
-            elseif notif == "AXResized" or notif == "AXMoved" then
-                module.triggerChange(app, win, module.events.framed)
+                if not axel:matchesCriteria("AXWindow") then
+                    return
+                end
+                local win = axel:asHSWindow()
+                -- check for all focus changes, or only for the condition below?
+                -- and app:kind() > 0
+                if notif == "AXFocusedWindowChanged" then
+                    module._updateAppWindows(app, ax)
+                    module.triggerChange(app, win, module.events.focused)
+                elseif notif == "AXResized" or notif == "AXMoved" then
+                    module.triggerChange(app, win, module.events.framed)
+                end
             end
-        end)
+        )
         w:start()
         module.observers[app:pid()] = w
     end
@@ -155,8 +173,7 @@ end
 module._updateSpaces = function()
     for _, space in pairs(spaces.layout()[spaces.mainScreenUUID()]) do
         for _, w in pairs(spaces.allWindowsForSpace(space)) do
-            module._addAppWindow(w:application(),
-                                 hs.axuielement.windowElement(w))
+            module._addAppWindow(w:application(), hs.axuielement.windowElement(w))
         end
     end
 end
@@ -169,49 +186,65 @@ end
 
 module.switcher = function()
     if module._chooser == nil then
-        module._chooser = hs.chooser.new(
-                              function(choice)
-                if choice ~= nil then choice.win:focus() end
-            end):bgDark(true):placeholderText("Switch to Window"):searchSubText(
-                              true)
+        module._chooser =
+            hs.chooser.new(
+            function(choice)
+                if choice ~= nil then
+                    choice.win:focus()
+                end
+            end
+        ):bgDark(true):placeholderText("Switch to Window"):searchSubText(true)
     elseif module._chooser:isVisible() then
         module._chooser:hide()
         return
     end
-    local windows = hs.fnutils.map(running.getWindows(), function(win)
-        return {
-            text = win:title(),
-            subText = win:application():title(),
-            image = hs.image.imageFromAppBundle(win:application():bundleID()),
-            win = win
-        }
-    end)
+    local windows =
+        hs.fnutils.map(
+        running.getWindows(),
+        function(win)
+            return {
+                text = win:title(),
+                subText = win:application():title(),
+                image = hs.image.imageFromAppBundle(win:application():bundleID()),
+                win = win
+            }
+        end
+    )
     module._chooser:choices(windows):show()
 end
 
-module._appWatcher = appw.new(function(appName, event, app)
-    if appName == nil then appName = "" end
-
-    if event == appw.launching then return end
-
-    if event == appw.terminated then
-        module.apps[app:pid()] = nil
-        if module.observers[app:pid()] ~= nil then
-            module.observers[app:pid()]:stop()
-            module.observers[app:pid()] = nil
+module._appWatcher =
+    appw.new(
+    function(appName, event, app)
+        if appName == nil then
+            appName = ""
         end
-        return
-    end
 
-    if module.apps[app:pid()] == nil then module._watchApp(app) end
+        if event == appw.launching then
+            return
+        end
 
-    if event == appw.activated or event == appw.launched then
-        local win = app:focusedWindow()
-        if win ~= nil then
-            module.triggerChange(app, win, module.events.focused)
+        if event == appw.terminated then
+            module.apps[app:pid()] = nil
+            if module.observers[app:pid()] ~= nil then
+                module.observers[app:pid()]:stop()
+                module.observers[app:pid()] = nil
+            end
+            return
+        end
+
+        if module.apps[app:pid()] == nil then
+            module._watchApp(app)
+        end
+
+        if event == appw.activated or event == appw.launched then
+            local win = app:focusedWindow()
+            if win ~= nil then
+                module.triggerChange(app, win, module.events.focused)
+            end
         end
     end
-end)
+)
 
 module._spaceWatcher = hs.spaces.watcher.new(module._updateRunning)
 
