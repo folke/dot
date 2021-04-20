fa08b41e8e4b598c23675e06f0ac3a61ed06b872
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-04-20 20:47:30 +0200

    async init of nvimtree

--- config/nvim/lua/config/tree.lua
+++ config/nvim/lua/config/tree.lua
@@ -7,5 +7,6 @@ vim.g.nvim_tree_auto_ignore_ft = { "dashboard", "startify" }
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_disable_netrw = 0
+vim.g.nvim_tree_lsp_diagnostics = 1
 
-require"nvim-tree".on_enter()
+require"nvim-tree.events".on_nvim_tree_ready(function() vim.cmd("NvimTreeRefresh") end)
