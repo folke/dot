e8d31b1dc2febed5eabec2564ab4a43d62a08282
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-04-25 22:33:56 +0200

    feat: zen mode!

--- config/nvim/lua/util.lua
+++ config/nvim/lua/util.lua
@@ -50,4 +50,47 @@ function M.float_terminal(cmd)
   vim.cmd(table.concat(autocmd, " "))
   vim.cmd [[startinsert]]
 end
+
+M.zen_win = nil
+
+function M.zen_mode()
+  if M.zen_win and vim.api.nvim_win_is_valid(M.zen_win) then
+    vim.api.nvim_win_close(M.zen_win, { force = true })
+    return
+  end
+  local vpad = 0
+  local hpad = math.floor(vim.o.columns * .2)
+
+  local bg_buf = vim.api.nvim_create_buf(false, true)
+  local bg_win = vim.api.nvim_open_win(bg_buf, false, {
+    relative = "editor",
+    width = vim.o.columns,
+    height = vim.o.lines,
+    focusable = false,
+    row = 1,
+    col = 1,
+    style = "minimal"
+  })
+  vim.api.nvim_win_set_option(bg_win, "winhighlight", "NormalFloat:Normal")
+
+  local buf = vim.api.nvim_get_current_buf()
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = vim.o.columns - hpad * 2,
+    height = vim.o.lines - vpad * 2,
+    row = vpad,
+    col = hpad
+    -- style = "minimal"
+  })
+  vim.api.nvim_win_set_option(win, "winhighlight", "NormalFloat:Normal")
+
+  local autocmd = {
+    "autocmd! WinClosed " .. win .. " ++once lua",
+    string.format("vim.api.nvim_win_close(%d, {force = true});", bg_win),
+    string.format("vim.api.nvim_buf_delete(%d, {force = true});", bg_buf)
+  }
+  vim.cmd(table.concat(autocmd, " "))
+  M.zen_win = win
+end
+
 return M
