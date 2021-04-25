b74430bf4bc492ce09a0ffde70e58f048f7034c5
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-04-25 16:01:42 +0200

    open floating terminal

--- config/nvim/lua/util.lua
+++ config/nvim/lua/util.lua
@@ -28,4 +28,26 @@ M.vnoremap = function(key, cmd, opts) return map("v", key, cmd, opts, { noremap 
 
 M.inoremap = function(key, cmd, opts) return map("i", key, cmd, opts, { noremap = true }) end
 
+function M.float_terminal(cmd)
+  local buf = vim.api.nvim_create_buf(false, true)
+  local vpad = 4
+  local hpad = 10
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = vim.o.columns - hpad * 2,
+    height = vim.o.lines - vpad * 2,
+    row = vpad,
+    col = hpad,
+    style = "minimal",
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+  })
+  vim.fn.termopen(cmd)
+  local autocmd = {
+    "autocmd! TermClose <buffer> lua",
+    string.format("vim.api.nvim_win_close(%d, {force = true});", win),
+    string.format("vim.api.nvim_buf_delete(%d, {force = true});", buf)
+  }
+  vim.cmd(table.concat(autocmd, " "))
+  vim.cmd [[startinsert]]
+end
 return M
