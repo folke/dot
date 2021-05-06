a49414f0f27580a7d7341cd333a9f66f507b064f
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-05-06 08:47:28 +0200

    feat: illuminate

--- config/nvim/lua/config/lsp/highlighting.lua
+++ config/nvim/lua/config/lsp/highlighting.lua
@@ -1,12 +1,13 @@
 return {
   setup = function(client) -- Set autocommands conditional on server_capabilities
     if client.resolved_capabilities.document_highlight then
+      require"illuminate".on_attach(client)
       vim.api.nvim_exec([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        " autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorHoldI <buffer> lua require('lspsaga.signaturehelp').signature_help()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        " autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]], false)
     end
