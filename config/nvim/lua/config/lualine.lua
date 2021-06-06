cdd34586cfa792b49114e325ebe407725fc65153
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-06 13:49:08 +0200

    feat(vim): added lsp progress

--- config/nvim/lua/config/lualine.lua
+++ config/nvim/lua/config/lualine.lua
@@ -2,6 +2,23 @@ local function clock()
   return " " .. os.date("%H:%M")
 end
 
+local function lsp_progress()
+  local messages = vim.lsp.util.get_progress_messages()
+  if #messages == 0 then
+    return
+  end
+  local status = {}
+  for _, msg in pairs(messages) do
+    table.insert(status, msg.percentage .. "%% " .. msg.title)
+  end
+  local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+  local ms = vim.loop.hrtime() / 1000000
+  local frame = math.floor(ms / 120) % #spinners
+  return table.concat(status, " | ") .. " " .. spinners[frame + 1]
+end
+
+vim.cmd([[autocmd User LspProgressUpdate let &ro = &ro]])
+
 local config = {
   options = {
     theme = "tokyonight",
@@ -15,7 +32,7 @@ local config = {
     lualine_a = { "mode" },
     lualine_b = { "branch" },
     lualine_c = { { "diagnostics", sources = { "nvim_lsp" } }, "filename" },
-    lualine_x = { "filetype" },
+    lualine_x = { "filetype", lsp_progress },
     lualine_y = { "progress" },
     lualine_z = { clock },
   },
