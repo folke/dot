eedfbbe3060cf5db77b347e714107de68d42f314
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-07 02:03:42 +0200

    feat(fish): nix env in fish

---
+++ config/fish/completions/replay.fish
@@ -0,0 +1,3 @@
+complete --command replay --no-files
+complete --command replay --exclusive --long version --description "Print version"
+complete --command replay --exclusive --long help --description "Print help"
---
+++ config/fish/conf.d/nix.fish
@@ -0,0 +1,1 @@
+replay source ~/.nix-profile/etc/profile.d/nix.sh
