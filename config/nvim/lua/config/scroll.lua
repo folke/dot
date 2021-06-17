18bf43af93580a05cbedc97530f2792e424ffbe6
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-17 11:38:21 +0200

    fix(neoscroll): set correct animation times

--- config/nvim/lua/config/scroll.lua
+++ config/nvim/lua/config/scroll.lua
@@ -1,21 +1,14 @@
 require("neoscroll").setup({})
 local map = {}
 
-local speed = 2
-
-local function t(v)
-  return tostring(math.ceil(v / speed))
-end
-
--- Syntax: t[keys] = {function, {function arguments}}
-map["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", t(8) } }
-map["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", t(8) } }
-map["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", t(7) } }
-map["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", t(7) } }
-map["<C-y>"] = { "scroll", { "-0.10", "false", t(20) } }
-map["<C-e>"] = { "scroll", { "0.10", "false", t(20) } }
-map["zt"] = { "zt", { t(7) } }
-map["zz"] = { "zz", { t(7) } }
-map["zb"] = { "zb", { t(7) } }
+map["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "80" } }
+map["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "80" } }
+map["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "250" } }
+map["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "250" } }
+map["<C-y>"] = { "scroll", { "-0.10", "false", "80" } }
+map["<C-e>"] = { "scroll", { "0.10", "false", "80" } }
+map["zt"] = { "zt", { "150" } }
+map["zz"] = { "zz", { "150" } }
+map["zb"] = { "zb", { "150" } }
 
 require("neoscroll.config").set_mappings(map)
