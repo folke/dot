a6a4653de7a9495178fe5bdcabe4b3e743b73ad7
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-22 15:05:01 +0200

    feat(hammerspoon): configurable margin for monocle mode

---
+++ hammerspoon/config.lua
@@ -0,0 +1,8 @@
+local options = {
+  topbar = 26,
+  monocle = {
+    margin = { 5, 10 },
+  },
+}
+
+return options
--- hammerspoon/monocle.lua
+++ hammerspoon/monocle.lua
@@ -1,9 +1,17 @@
 local module = { state = {} }
+local config = require("config")
 
 module.toggle =
   ---@param win hs.window
   function(win)
     local screenFrame = win:screen():frame()
+
+    screenFrame = hs.geometry.new({
+      screenFrame.x + config.monocle.margin[1],
+      screenFrame.y + config.topbar + config.monocle.margin[2],
+      screenFrame.w - 2 * config.monocle.margin[1],
+      screenFrame.h - config.topbar - 2 * config.monocle.margin[2],
+    })
 
     if win:frame():equals(screenFrame) then
       -- restore state
