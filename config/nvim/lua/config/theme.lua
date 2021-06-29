f93829ddc52aa7f246470fbe64d4c72646c4c944
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-29 10:01:52 +0200

    perf(vim): load tokyonight with lua

--- config/nvim/lua/config/theme.lua
+++ config/nvim/lua/config/theme.lua
@@ -46,4 +46,5 @@ vim.g.tokyonight_dark_float = true
 vim.g.tokyonight_colors = {}
 -- vim.g.tokyonight_colors = { border = "orange" }
 
-vim.cmd("colorscheme tokyonight") -- Put your favorite colorscheme here
+require("tokyonight").colorscheme()
+-- vim.cmd("colorscheme tokyonight") -- Put your favorite colorscheme here
