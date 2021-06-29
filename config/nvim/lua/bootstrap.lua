69edf2cd5a4fd94b4be37c0f68f94b428de505de
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-29 11:36:45 +0200

    chore: cleanup

--- config/nvim/lua/bootstrap.lua
+++ config/nvim/lua/bootstrap.lua
@@ -1,11 +1,10 @@
-local execute = vim.api.nvim_command
 local fn = vim.fn
 
 local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
-  execute("packadd packer.nvim")
+  vim.cmd("packadd packer.nvim")
 end
 
 vim.cmd([[packadd packer.nvim]])
