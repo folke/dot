7d164d133a2d9796e023bee30704bf4abcd5d6ab
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-29 11:37:13 +0200

    perf(vim): lazy-load packer since real loading is in packer_compiled.vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,10 @@
-require "util"
-require "options"
-require "bootstrap"
-require "plugins"
-require "config.theme"
+require("util")
+require("options")
+
+-- no need to load this immediately, since we have packer_compiled
+vim.defer_fn(function()
+  require("bootstrap")
+  require("plugins")
+end, 0)
+
+require("config.theme")
