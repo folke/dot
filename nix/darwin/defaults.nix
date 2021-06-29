91a3f02071810b561150423ed496cb70e7a261a8
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-06-29 22:37:10 +0200

    fix(nix): slower key repeat

--- nix/darwin/defaults.nix
+++ nix/darwin/defaults.nix
@@ -1,8 +1,8 @@
 { config, lib, ... }:
 with lib;{
   system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
-  system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
-  system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 20;
+  system.defaults.NSGlobalDomain.KeyRepeat = 2;
   system.defaults.dock.autohide = false;
   system.defaults.dock.autohide-delay = "0.0";
   system.defaults.dock.autohide-time-modifier = "0.4";
