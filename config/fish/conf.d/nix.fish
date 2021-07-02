c64b96f8fcada1584f3a1808e0e55d0ff2123f25
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-07-02 14:02:59 +0200

    fix(nix): properly source nix env

--- config/fish/conf.d/nix.fish
+++ config/fish/conf.d/nix.fish
@@ -1,3 +1,9 @@
-# replay source ~/.nix-profile/etc/profile.d/nix.sh
+set -l files ~/.nix-profile/etc/profile.d/nix.sh  /etc/profiles/per-user/folke/etc/profile.d/hm-session-vars.sh ~/.nix-profile/etc/profile.d/hm-session-vars.sh
 # replay source /etc/static/profiles/per-user/folke/etc/profile.d/nix.sh
-replay source /etc/profiles/per-user/folke/etc/profile.d/hm-session-vars.sh
+
+set -e __HM_SESS_VARS_SOURCED
+for f in $files
+  test -f $f
+  and echo $f
+  and replay source $f
+end
