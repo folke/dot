f35c9ef0bf9ab5326606d1aebb027dcd9d007a67
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-07-18 09:06:02 +0200

    feat: util.lsp_config -> shows settings for attached lsp clients

--- config/nvim/lua/util/init.lua
+++ config/nvim/lua/util/init.lua
@@ -167,4 +167,12 @@ function M.docs()
   docgen.generate_readme(metadata)
 end
 
+function M.lsp_config()
+  local ret = {}
+  for _, client in pairs(vim.lsp.get_active_clients()) do
+    ret[client.name] = { root_dir = client.config.root_dir, settings = client.config.settings }
+  end
+  dump(ret)
+end
+
 return M
