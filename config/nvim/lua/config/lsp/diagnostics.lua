66653ec76ca47989316d0814cf4c7e4f2a64910d
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-10-21 11:49:18 +0200

    fix(vim): fixed diagnostic signs

--- config/nvim/lua/config/lsp/diagnostics.lua
+++ config/nvim/lua/config/lsp/diagnostics.lua
@@ -6,9 +6,9 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
   severity_sort = true,
 })
 
-local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
+  local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
