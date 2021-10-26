93d1d73845043016f44518f51c37d2d8d0a0b2fc
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-10-26 09:03:06 +0200

    feat(fish): added code to update my projects

--- config/fish/functions/projects.fish
+++ config/fish/functions/projects.fish
@@ -1,12 +1,22 @@
+set myprojects which-key.nvim persistence.nvim workspace.nvim tokyonight.nvim trouble.nvim zen-mode.nvim lsp-colors.nvim todo-comments.nvim lua-dev.nvim twilight.nvim ultra-runner
 
 function projects
-  set myprojects which-key.nvim persistence.nvim workspace.nvim tokyonight.nvim trouble.nvim zen-mode.nvim lsp-colors.nvim todo-comments.nvim lua-dev.nvim twilight.nvim
-
-  for p in $myprojects
-    set fp ~/projects/$p
-    if test ! -d $fp
-      echo "[clone] $fp"
-      git clone git@github.com:folke/$p.git $fp
+    for p in $myprojects
+        set fp ~/projects/$p
+        if test ! -d $fp
+            echo "[clone] $fp"
+            git clone git@github.com:folke/$p.git $fp
+        end
     end
-  end
+end
+
+function projects_update
+    projects
+    for p in $myprojects
+        set fp ~/projects/$p
+        echo "[$fp]"
+        if test -d $fp
+            git -C $fp pull
+        end
+    end
 end
