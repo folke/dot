f736dda67e9023d9ecb1e62d8a5f959a4ccb14cf
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-10-26 15:22:50 +0200

    chore(fish): removed fisher

--- config/fish/completions/replay.fish
+++
@@ -1,3 +0,0 @@
-complete --command replay --no-files
-complete --command replay --exclusive --long version --description "Print version"
-complete --command replay --exclusive --long help --description "Print help"
