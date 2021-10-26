f273266a148271af78f8b370dedcc7214de35215
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-10-26 09:06:20 +0200

    refactor(vim): removed lsp.config

--- config/nvim/lua/config/lsp/init.lua
+++ config/nvim/lua/config/lsp/init.lua
@@ -1,49 +1,8 @@
 local util = require("util")
 local lspconfig = require("lspconfig")
 
-if vim.lsp.setup then
-  vim.lsp.setup({
-    floating_preview = { border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" } },
-    diagnostics = {
-      signs = { error = " ", warning = " ", hint = " ", information = " " },
-      display = {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = { spacing = 4, prefix = "●" },
-        severity_sort = true,
-      },
-    },
-    completion = {
-      kind = {
-        Class = " ",
-        Color = " ",
-        Constant = " ",
-        Constructor = " ",
-        Enum = "了 ",
-        EnumMember = " ",
-        Field = " ",
-        File = " ",
-        Folder = " ",
-        Function = " ",
-        Interface = "ﰮ ",
-        Keyword = " ",
-        Method = "ƒ ",
-        Module = " ",
-        Property = " ",
-        Snippet = "﬌ ",
-        Struct = " ",
-        Text = " ",
-        Unit = " ",
-        Value = " ",
-        Variable = " ",
-      },
-    },
-  })
-else
-  -- require("config.lsp.saga")
-  require("config.lsp.diagnostics")
-  require("config.lsp.kind").setup()
-end
+require("config.lsp.diagnostics")
+require("config.lsp.kind").setup()
 
 local function on_attach(client, bufnr)
   require("config.lsp.formatting").setup(client, bufnr)
