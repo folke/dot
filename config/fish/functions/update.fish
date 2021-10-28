15686b0645f16ea49d3d652dfced5e4d40f37cc6
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-10-28 11:51:42 +0200

    chore: disabled pip3 update

--- config/fish/functions/update.fish
+++ config/fish/functions/update.fish
@@ -17,8 +17,8 @@ function update --description 'Update homebrew, fish, pnpm'
     and fnm_update
     and pnpm update -g
 
-    and echo "[update] python"
-    and pip3 list -o --user --format=freeze | sed "s/==.*//" | xargs pip3 install -U --user
+    # and echo "[update] python"
+    # and pip3 list -o --user --format=freeze | sed "s/==.*//" | xargs pip3 install -U --user
 
     and echo "[update] tldr"
     and tldr -u
