b096b8e74de546aa15ce5b130616a44e3ab8c626
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-10-28 11:52:46 +0200

    perf(vim): speed up filetype detection

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -23,6 +23,7 @@ local config = {
 local function plugins(use)
   -- Packer can manage itself as an optional plugin
   use({ "wbthomason/packer.nvim", opt = true })
+  use({ "nathom/filetype.nvim" })
   use({ "folke/workspace.nvim" })
   -- LSP
   use({
