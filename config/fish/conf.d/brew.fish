36a193f6e34335df642b809096842799e56ed032
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-11-04 19:59:30 +0100

    fix(fish): move linuxbrew to end of path

--- config/fish/conf.d/brew.fish
+++ config/fish/conf.d/brew.fish
@@ -1,8 +1,11 @@
 
-set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
-set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
-set -gx HOMEBREW_SHELLENV_PREFIX "/home/linuxbrew/.linuxbrew";
-set -q PATH; or set PATH ''; set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
+set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
+set -gx HOMEBREW_SHELLENV_PREFIX "/home/linuxbrew/.linuxbrew"
+set -q PATH; or set PATH ''
+set -gx PATH $PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin"
+set -q MANPATH; or set MANPATH ''
+set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
+set -q INFOPATH; or set INFOPATH ''
+set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
