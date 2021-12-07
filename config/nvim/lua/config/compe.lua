f754ec81103d4b52b21bcd2b30f6e55350ed1769
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-12-07 08:14:42 +0100

    feat(nvim): better cmp integration

--- config/nvim/lua/config/compe.lua
+++ config/nvim/lua/config/compe.lua
@@ -6,6 +6,9 @@ vim.o.completeopt = "menuone,noselect"
 local cmp = require("cmp")
 
 cmp.setup({
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+  },
   snippet = {
     expand = function(args)
       -- For `luasnip` user.
@@ -34,10 +37,45 @@ cmp.setup({
   },
   experimental = {
     ghost_text = {
-      hl_group = "LineNr",
+      hl_group = "LspCodeLens",
+    },
+  },
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      -- cmp.config.compare.kind,
+      -- cmp.config.compare.sort_text,
+      -- cmp.config.compare.length,
+      cmp.config.compare.order,
     },
   },
 })
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+
+-- local types = cmp.types
+-- cmp.config.compare.kind = function(entry1, entry2)
+--   local kind1 = entry1:get_kind()
+--   kind1 = kind1 == types.lsp.CompletionItemKind.Text and 100 or kind1
+--   local kind2 = entry2:get_kind()
+--   kind2 = kind2 == types.lsp.CompletionItemKind.Text and 100 or kind2
+--   if kind1 ~= kind2 then
+--     if kind1 == types.lsp.CompletionItemKind.Snippet then
+--       return false
+--     end
+--     if kind2 == types.lsp.CompletionItemKind.Snippet then
+--       return true
+--     end
+--     local diff = kind1 - kind2
+--     if diff < 0 then
+--       return true
+--     elseif diff > 0 then
+--       return false
+--     end
+--   end
+-- end
 
 -- require("compe").setup({
 --   enabled = true,
