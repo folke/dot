62ef28ec7b73953fd716c67663fe70743636ba63
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-12-08 23:07:16 +0100

    fix(vim): use lsp sorting for cmp

--- config/nvim/lua/config/compe.lua
+++ config/nvim/lua/config/compe.lua
@@ -42,8 +42,8 @@ cmp.setup({
   },
   sorting = {
     comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
+      -- cmp.config.compare.offset,
+      -- cmp.config.compare.exact,
       cmp.config.compare.score,
       -- cmp.config.compare.kind,
       -- cmp.config.compare.sort_text,
