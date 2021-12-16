e6d4a725f1e6fca3d943d57897395e3b4b06093c
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-12-16 13:43:49 +0100

    feat(vim): use thick style for bufferline

--- config/nvim/lua/config/bufferline.lua
+++ config/nvim/lua/config/bufferline.lua
@@ -19,7 +19,7 @@ require("bufferline").setup({
     show_close_icon = true,
     diagnostics = "nvim_lsp",
     always_show_bufferline = false,
-    separator_style = "slant",
+    separator_style = "thick",
     diagnostics_indicator = function(_, _, diag)
       local s = {}
       for _, severity in ipairs(severities) do
