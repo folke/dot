8d20c6b16d805a4dcda74ed7ac937731893d4af8
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-12-16 14:19:02 +0100

    feat(vim): auto reload packer (still buggy)

--- config/nvim/lua/util/packer.lua
+++ config/nvim/lua/util/packer.lua
@@ -13,6 +13,12 @@ function M.bootstrap()
   end
   vim.cmd([[packadd packer.nvim]])
   -- vim.cmd("autocmd BufWritePost plugins.lua PackerCompile") -- Auto compile when there are changes in plugins.lua
+  vim.cmd([[
+    augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    augroup end
+  ]])
 end
 
 function M.get_name(pkg)
