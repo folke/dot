87cff66811ccdd9f53dfd2cd6616c7f03d5bbee7
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-12-16 13:43:35 +0100

    fix(vim): fix null-ls .nvim.settings.json root pattern

--- config/nvim/lua/config/lsp/null-ls.lua
+++ config/nvim/lua/config/lsp/null-ls.lua
@@ -17,6 +17,7 @@ function M.setup(options)
       -- nls.builtins.code_actions.gitsigns,
     },
     on_attach = options.on_attach,
+    root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".nvim.settings.json", ".git"),
   })
 end
 
