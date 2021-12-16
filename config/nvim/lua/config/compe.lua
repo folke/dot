bd78609c91b83fdff06f645482a11b290cc7b427
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-12-16 13:44:17 +0100

    fix(vim): fixed compe sorting for lsp

--- config/nvim/lua/config/compe.lua
+++ config/nvim/lua/config/compe.lua
@@ -42,11 +42,11 @@ cmp.setup({
   },
   sorting = {
     comparators = {
-      -- cmp.config.compare.offset,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.offset,
       -- cmp.config.compare.exact,
       cmp.config.compare.score,
       -- cmp.config.compare.kind,
-      -- cmp.config.compare.sort_text,
       -- cmp.config.compare.length,
       cmp.config.compare.order,
     },
