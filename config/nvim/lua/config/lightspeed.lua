cf71e08c5038915c00121add55e9b0f557399ecb
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-12-17 16:37:09 +0100

    feat(vim): lightspeed config

--- config/nvim/lua/config/lightspeed.lua
+++ config/nvim/lua/config/lightspeed.lua
@@ -1,16 +1,20 @@
 require("lightspeed").setup({
-  -- jump_to_first_match = true,
-  -- jump_on_partial_input_safety_timeout = 400,
-  -- -- This can get _really_ slow if the window has a lot of content,
-  -- -- turn it on only if your machine can always cope with it.
-  -- highlight_unique_chars = false,
+  -- exit_after_idle_msecs = { labeled = nil, unlabeled = 1000 },
+  --
+  -- -- s/x
   -- grey_out_search_area = true,
+  -- highlight_unique_chars = true,
   -- match_only_the_start_of_same_char_seqs = true,
-  -- limit_ft_matches = 5,
-  -- -- full_inclusive_prefix_key = '<c-x>',
-  -- -- By default, the values of these will be decided at runtime,
-  -- -- based on `jump_to_first_match`.
-  -- labels = nil,
-  -- cycle_group_fwd_key = nil,
-  -- cycle_group_bwd_key = nil,
+  -- jump_on_partial_input_safety_timeout = 400,
+  -- substitute_chars = { ['\r'] = '¬' },
+  -- -- Leaving the appropriate list empty effectively disables
+  -- -- "smart" mode, and forces auto-jump to be on or off.
+  -- safe_labels = { ... },
+  -- labels = { ... },
+  -- cycle_group_fwd_key = '<space>',
+  -- cycle_group_bwd_key = '<tab>',
+  --
+  -- -- f/t
+  -- limit_ft_matches = 4,
+  repeat_ft_with_target_char = true,
 })
