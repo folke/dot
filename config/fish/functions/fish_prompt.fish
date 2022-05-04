4b5cb690e19e10ee816733fc7ec2842ada35ff31
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-05-04 12:16:01 +0200

    fix: updated starship prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -5,10 +5,27 @@ function fish_prompt
         case '*'
             set STARSHIP_KEYMAP insert
     end
+    set STARSHIP_CMD_PIPESTATUS $pipestatus
     set STARSHIP_CMD_STATUS $status
     # Account for changes in variable name between v2.7 and v3.0
     set STARSHIP_DURATION "$CMD_DURATION$cmd_duration"
-    starship prompt --status=$STARSHIP_CMD_STATUS --keymap=$STARSHIP_KEYMAP --cmd-duration=$STARSHIP_DURATION --jobs=(count (jobs -p))
+    set STARSHIP_JOBS (count (jobs -p))
+    /usr/bin/starship prompt --terminal-width="$COLUMNS" --status=$STARSHIP_CMD_STATUS --pipestatus="$STARSHIP_CMD_PIPESTATUS" --keymap=$STARSHIP_KEYMAP --cmd-duration=$STARSHIP_DURATION --jobs=$STARSHIP_JOBS
+end
+
+function fish_right_prompt
+    switch "$fish_key_bindings"
+        case fish_hybrid_key_bindings fish_vi_key_bindings
+            set STARSHIP_KEYMAP "$fish_bind_mode"
+        case '*'
+            set STARSHIP_KEYMAP insert
+    end
+    set STARSHIP_CMD_PIPESTATUS $pipestatus
+    set STARSHIP_CMD_STATUS $status
+    # Account for changes in variable name between v2.7 and v3.0
+    set STARSHIP_DURATION "$CMD_DURATION$cmd_duration"
+    set STARSHIP_JOBS (count (jobs -p))
+    /usr/bin/starship prompt --right --terminal-width="$COLUMNS" --status=$STARSHIP_CMD_STATUS --pipestatus="$STARSHIP_CMD_PIPESTATUS" --keymap=$STARSHIP_KEYMAP --cmd-duration=$STARSHIP_DURATION --jobs=$STARSHIP_JOBS
 end
 
 # Disable virtualenv prompt, it breaks starship
@@ -17,7 +34,7 @@ set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 # Remove default mode prompt
 builtin functions -e fish_mode_prompt
 
-set -gx STARSHIP_SHELL fish
+set -gx STARSHIP_SHELL "fish"
 
 # Set up the session key that will be used to store logs
 set -gx STARSHIP_SESSION_KEY (random 10000000000000 9999999999999999)
