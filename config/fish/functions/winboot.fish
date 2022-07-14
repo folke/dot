dde64724f212dba591945ef66178eefa4a25de7c
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-07-14 19:38:49 +0200

    feat: winboot

--- config/fish/functions/winboot.fish
+++ config/fish/functions/winboot.fish
@@ -1,3 +1,3 @@
-function winboot --wraps='grub2-reboot "Windows Boot Manager (on /dev/nvme0n1p1)"; sudo reboot' --description 'alias winboot grub2-reboot "Windows Boot Manager (on /dev/nvme0n1p1)"; sudo reboot'
-  grub2-reboot "Windows Boot Manager (on /dev/nvme0n1p1)"; sudo reboot $argv; 
+function winboot --wraps='sudo grub2-reboot "Windows Boot Manager (on /dev/nvme0n1p1)"; sudo reboot' --description 'alias winboot sudo grub2-reboot "Windows Boot Manager (on /dev/nvme0n1p1)"; sudo reboot'
+  sudo grub2-reboot "Windows Boot Manager (on /dev/nvme0n1p1)"; sudo reboot $argv; 
 end
