da0e28e0cec16f20bca0e526918da35d7cb6b545
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-07-14 19:39:07 +0200

    fix: gnome-keyring is now started as a system service

--- config/fish/conf.d/gnome.fish
+++ config/fish/conf.d/gnome.fish
@@ -1,3 +1,5 @@
-if test -n "$DESKTOP_SESSION"
-    set -x (gnome-keyring-daemon --start | string split "=")
-end
+# if test -n "$DESKTOP_SESSION"
+#     for env_var in (gnome-keyring-daemon --start)
+#         set -x (echo $env_var | string split "=")
+#     end
+# end
