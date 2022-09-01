c1274e78c5afb0cda70ca274c5bc7774225a6a21
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-01 12:36:48 +0200

    chore: cleanup

--- config/nvim/lua/config/lsp/init.lua
+++ config/nvim/lua/config/lsp/init.lua
@@ -9,9 +9,6 @@ local function on_attach(client, bufnr)
   require("nvim-navic").attach(client, bufnr)
   require("config.lsp.formatting").setup(client, bufnr)
   require("config.lsp.keys").setup(client, bufnr)
-  -- require("config.lsp.completion").setup(client, bufnr)
-  require("config.lsp.highlighting").setup(client)
-
 end
 
 local servers = {
