82535e425bb110f9af2526a6ae386a06bd731db2
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-01 21:39:37 +0200

    feat: auto create directories when saving a file

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -115,6 +115,13 @@ cmd([[
   autocmd InsertEnter,WinLeave * set nocursorline
 ]])
 
+-- create directories when needed, when saving a file
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  command = [[call mkdir(expand('<afile>:p:h'), 'p')]],
+})
+
+-- Fix conceallevel for json files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = "json",
   callback = function()
