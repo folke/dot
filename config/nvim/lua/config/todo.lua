529271f413cadaf2a2bc7f32387d7f42f1d83337
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-06 16:02:28 +0200

    feat: added ]t and [t for todo comments

--- config/nvim/lua/config/todo.lua
+++ config/nvim/lua/config/todo.lua
@@ -1,1 +1,9 @@
 require("todo-comments").setup({ keywords = { TODO = { alt = { "WIP" } } } })
+
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
