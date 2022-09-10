577dedbf1edba4d9c43a9c7397d26f838c55d8c5
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-10 20:25:11 +0200

    feat: handy debug method to show errors of pcall usage

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,17 @@
+local function p(fn, ...)
+  local args = { ... }
+  return xpcall(fn and function()
+    return fn(unpack(args))
+  end, function(err)
+    if err:find("DevIcon") or err:find("mason") or err:find("Invalid highlight") then
+      return err
+    end
+    vim.api.nvim_echo({ { err, "ErrorMsg" }, { debug.traceback("", 3), "Normal" } }, true, {})
+    return err
+  end)
+end
+
+-- _G.pcall = p
 require("util")
 require("options")
 
