f682044d0b7486b8ef769f6243c04c5d7b9f8635
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-18 10:12:17 +0200

    feat: added dumpp to show inspect in notify markdown window

--- config/nvim/lua/util/init.lua
+++ config/nvim/lua/util/init.lua
@@ -1,6 +1,19 @@
 -- selene: allow(global_usage)
 _G.dump = function(...)
-  print(vim.inspect(...))
+  vim.pretty_print(...)
+end
+
+_G.dumpp = function(...)
+  local msg = vim.inspect(...)
+  vim.notify("```lua\n" .. msg .. "\n```", vim.log.levels.INFO, {
+    title = "Debug",
+    on_open = function(win)
+      vim.api.nvim_win_set_option(win, "conceallevel", 3)
+      local buf = vim.api.nvim_win_get_buf(win)
+      vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+      vim.api.nvim_win_set_option(win, "spell", false)
+    end,
+  })
 end
 
 -- selene: allow(global_usage)
