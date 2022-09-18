f3c1d130e7c0bf21913372c6c80e93bc33b8a976
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-18 11:37:33 +0200

    feat: better logging with vim.notify

--- config/nvim/lua/util/init.lua
+++ config/nvim/lua/util/init.lua
@@ -55,22 +55,16 @@ function M.t(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-function M.log(msg, hl, name)
-  name = name or "Neovim"
-  hl = hl or "Todo"
-  vim.api.nvim_echo({ { name .. ": ", hl }, { msg } }, true, {})
-end
-
 function M.warn(msg, name)
-  vim.notify(msg, vim.log.levels.WARN, { title = name })
+  vim.notify(msg, vim.log.levels.WARN, { title = name or "init.lua" })
 end
 
 function M.error(msg, name)
-  vim.notify(msg, vim.log.levels.ERROR, { title = name })
+  vim.notify(msg, vim.log.levels.ERROR, { title = name or "init.lua" })
 end
 
 function M.info(msg, name)
-  vim.notify(msg, vim.log.levels.INFO, { title = name })
+  vim.notify(msg, vim.log.levels.INFO, { title = name or "init.lua" })
 end
 
 function M.toggle(option, silent)
