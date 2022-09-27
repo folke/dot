8a2a807184d4c7a69b18066d7bb5668468d54f9e
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-27 20:15:40 +0200

    feat: use new WhichKey mappings schema

--- config/nvim/lua/plugins/lsp/keys.lua
+++ config/nvim/lua/plugins/lsp/keys.lua
@@ -1,79 +1,67 @@
 local wk = require("which-key")
-local util = require("util")
 
 local M = {}
 
-function M.setup(client, bufnr)
-  -- Mappings.
-  local opts = { noremap = true, silent = true, buffer = bufnr }
+function M.setup(client, buffer)
+  local cap = client.server_capabilities
 
   local keymap = {
-    c = {
-      name = "+code",
-      r = { vim.lsp.buf.rename, "Rename" },
-      a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
-      d = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Line Diagnostics" },
-      l = {
-        name = "+lsp",
-        i = { "<cmd>LspInfo<cr>", "Lsp Info" },
-        a = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Folder" },
-        r = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Folder" },
+    buffer = buffer,
+    ["<leader>"] = {
+      c = {
+        name = "+code",
+        {
+          cond = client.name == "tsserver",
+          o = { "<cmd>:TypescriptOrganizeImports<CR>", "Organize Imports" },
+          R = { "<cmd>:TypescriptRenameFile<CR>", "Rename File" },
+        },
+        r = { vim.lsp.buf.rename, "Rename", cond = cap.renameProvider },
+        a = {
+          { vim.lsp.buf.code_action, "Code Action" },
+          { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action", mode = "v" },
+        },
+        f = {
+          { require("plugins.lsp.formatting").format, "Format Document", cond = cap.documentFormatting },
+          {
+            require("plugins.lsp.formatting").format,
+            "Format Range",
+            cond = cap.documentRangeFormatting,
+            mode = "v",
+          },
+        },
+        d = { vim.diagnostic.open_float, "Line Diagnostics" },
         l = {
-          "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-          "List Folders",
+          name = "+lsp",
+          i = { "<cmd>LspInfo<cr>", "Lsp Info" },
+          a = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Folder" },
+          r = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Folder" },
+          l = { "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "List Folders" },
         },
       },
+      x = {
+        d = { "<cmd>Telescope diagnostics<cr>", "Search Diagnostics" },
+      },
     },
-    x = {
-      d = { "<cmd>Telescope diagnostics<cr>", "Search Diagnostics" },
+    g = {
+      name = "+goto",
+      d = { "<cmd>Telescope lsp_definitions<cr>", "Goto Definition" },
+      r = { "<cmd>Telescope lsp_references<cr>", "References" },
+      R = { "<cmd>Trouble lsp_references<cr>", "Trouble References" },
+      D = { "<cmd>Telescope lsp_declarations<CR>", "Goto Declaration" },
+      s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
+      I = { "<cmd>Telescope lsp_implementations<CR>", "Goto Implementation" },
+      t = { "<cmd>Telescope lsp_type_definitions<cr>", "Goto Type Definition" },
     },
+    ["K"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
+    ["[d"] = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "Next Diagnostic" },
+    ["]d"] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Prev Diagnostic" },
+    ["[e"] = { "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>", "Next Error" },
+    ["]e"] = { "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>", "Prev Error" },
+    ["[w"] = { "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.WARNING})<CR>", "Next Warning" },
+    ["]w"] = { "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.WARNING})<CR>", "Prev Warning" },
   }
 
-  if not client.server_capabilities.renameProvider then
-    keymap.c.r = nil
-  end
-
-  if client.name == "tsserver" then
-    keymap.c.o = { "<cmd>:TypescriptOrganizeImports<CR>", "Organize Imports" }
-    keymap.c.R = { "<cmd>:TypescriptRenameFile<CR>", "Rename File" }
-  end
-
-  local keymap_visual = {
-    c = {
-      name = "+code",
-      a = { ":<C-U>lua vim.lsp.buf.range_code_action()<CR>", "Code Action" },
-    },
-  }
-
-  local keymap_goto = {
-    name = "+goto",
-    d = { "<cmd>Telescope lsp_definitions<cr>", "Goto Definition" },
-    r = { "<cmd>Telescope lsp_references<cr>", "References" },
-    R = { "<cmd>Trouble lsp_references<cr>", "Trouble References" },
-    D = { "<Cmd>Telescope lsp_declarations<CR>", "Goto Declaration" },
-    s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
-    I = { "<cmd>Telescope lsp_implementations<CR>", "Goto Implementation" },
-    t = { "<cmd>Telescope lsp_type_definitions<cr>", "Goto Type Definition" },
-  }
-
-  vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-  vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-  vim.keymap.set("n", "[e", "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>", opts)
-  vim.keymap.set("n", "]e", "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>", opts)
-  vim.keymap.set("n", "[w", "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.WARNING})<CR>", opts)
-  vim.keymap.set("n", "]w", "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.WARNING})<CR>", opts)
-
-  -- Set some keybinds conditional on server capabilities
-  if client.server_capabilities.documentFormatting then
-    keymap.c.f = { "<cmd>lua vim.lsp.buf.formatting()<CR>", "Format Document" }
-  elseif client.server_capabilities.documentRangeFormatting then
-    keymap_visual.c.f = { "<cmd>lua vim.lsp.buf.range_formatting()<CR>", "Format Range" }
-  end
-
-  wk.register(keymap, { buffer = bufnr, prefix = "<leader>" })
-  wk.register(keymap_visual, { buffer = bufnr, prefix = "<leader>", mode = "v" })
-  wk.register(keymap_goto, { buffer = bufnr, prefix = "g" })
+  wk.register(keymap)
 end
 
 return M
