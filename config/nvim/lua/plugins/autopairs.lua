bd76bd8d6b40871ea0015ee30e627b1314bf80bc
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-27 23:46:43 +0200

    feat: added ts support to autopairs

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -2,6 +2,11 @@ return {
   module = "nvim-autopairs",
   config = function()
     local npairs = require("nvim-autopairs")
-    npairs.setup()
+    npairs.setup({
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "comment" }, -- it will not add a pair on that treesitter node
+      },
+    })
   end,
 }
