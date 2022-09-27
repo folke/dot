74aa9c7746f04ee49fc9de685ab4ef8ba04b9281
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-27 20:09:27 +0200

    fix(fish): update script. enable brew again

--- config/fish/functions/update.fish
+++ config/fish/functions/update.fish
@@ -1,9 +1,9 @@
 # Defined in /Users/folke/.config/fish/config.fish @ line 71
 function update --description 'Update homebrew, fish, pnpm'
     ~
-    # and echo "[update] Homebrew"
-    # and brew update
-    # and brew upgrade
+    and echo "[update] Homebrew"
+    and brew update
+    and brew upgrade
 
     # and echo "[update] cleaning brew cache"
     # #and rm -rfv (brew --cache) # brew cleanup -s doesn't remove everythin
@@ -21,7 +21,7 @@ function update --description 'Update homebrew, fish, pnpm'
     # and pip3 list -o --user --format=freeze | sed "s/==.*//" | xargs pip3 install -U --user
 
     and echo "[update] tldr"
-    and tldr -u
+    and command tldr -u
 
     and echo "[update] tmux"
     and tmux_update
