9a504d11db46717357e1d85c27d3a76697f5d584
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-27 20:09:12 +0200

    fix(fzf): don't limit matches

--- config/fish/functions/__fzf_history.fish
+++ config/fish/functions/__fzf_history.fish
@@ -2,7 +2,7 @@
 function __fzf_history --description "Search command history using fzf. Replace the commandline with the selected command."
     # history merge incorporates history changes from other fish sessions
     history merge
-    history --null -n1000 \
+    history --null \
         | string replace -ar "\x0" ";__delim__;" \
         | fish_indent_ansi -i \
         | sed "s/__delim__/\\x0/g" \
