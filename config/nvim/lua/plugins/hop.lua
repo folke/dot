b82b90b14da12b43a00f22d557fc5913ce776fe3
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-28 21:47:09 +0200

    feat: better config for Hop

--- config/nvim/lua/plugins/hop.lua
+++ config/nvim/lua/plugins/hop.lua
@@ -11,24 +11,36 @@ function M.init()
   -- place this in one of your configuration file(s)
   vim.keymap.set("", "gh", "<cmd>:HopWord<cr>", { desc = "Hop Word" })
 
+  vim.keymap.set("", "s", function()
+    require("hop").hint_char2({
+      direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+    })
+  end, {})
+
+  vim.keymap.set("", "S", function()
+    require("hop").hint_char2({
+      direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+    })
+  end, {})
+
   vim.keymap.set("", "f", function()
     require("hop").hint_char1({
       direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-      current_line_only = true,
+      -- current_line_only = true,
     })
   end, {})
 
   vim.keymap.set("", "F", function()
     require("hop").hint_char1({
       direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-      current_line_only = true,
+      -- current_line_only = true,
     })
   end, {})
 
   vim.keymap.set("", "t", function()
     require("hop").hint_char1({
       direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-      current_line_only = true,
+      -- current_line_only = true,
       hint_offset = -1,
     })
   end, {})
@@ -36,7 +48,7 @@ function M.init()
   vim.keymap.set("", "T", function()
     require("hop").hint_char1({
       direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-      current_line_only = true,
+      -- current_line_only = true,
       hint_offset = 1,
     })
   end, {})
