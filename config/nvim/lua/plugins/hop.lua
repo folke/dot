7c7aec78558456d03693b5c479db550066bbd5c0
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-02 16:11:06 +0200

    feat: added mini.jump to replace Hop single char searches

--- config/nvim/lua/plugins/hop.lua
+++ config/nvim/lua/plugins/hop.lua
@@ -11,47 +11,47 @@ function M.init()
   -- place this in one of your configuration file(s)
   vim.keymap.set("", "gh", "<cmd>:HopWord<cr>", { desc = "Hop Word" })
 
-  vim.keymap.set("", "s", function()
-    require("hop").hint_char2({
-      direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-    })
-  end, {})
-
-  vim.keymap.set("", "S", function()
-    require("hop").hint_char2({
-      direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-    })
-  end, {})
-
-  vim.keymap.set("", "f", function()
-    require("hop").hint_char1({
-      direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-      -- current_line_only = true,
-    })
-  end, {})
-
-  vim.keymap.set("", "F", function()
-    require("hop").hint_char1({
-      direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-      -- current_line_only = true,
-    })
-  end, {})
-
-  vim.keymap.set("", "t", function()
-    require("hop").hint_char1({
-      direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-      -- current_line_only = true,
-      hint_offset = -1,
-    })
-  end, {})
-
-  vim.keymap.set("", "T", function()
-    require("hop").hint_char1({
-      direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
-      -- current_line_only = true,
-      hint_offset = 1,
-    })
-  end, {})
+  -- vim.keymap.set("", "s", function()
+  --   require("hop").hint_char2({
+  --     direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+  --   })
+  -- end, {})
+  --
+  -- vim.keymap.set("", "S", function()
+  --   require("hop").hint_char2({
+  --     direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+  --   })
+  -- end, {})
+  --
+  -- vim.keymap.set("", "f", function()
+  --   require("hop").hint_char1({
+  --     direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+  --     -- current_line_only = true,
+  --   })
+  -- end, {})
+  --
+  -- vim.keymap.set("", "F", function()
+  --   require("hop").hint_char1({
+  --     direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+  --     -- current_line_only = true,
+  --   })
+  -- end, {})
+  --
+  -- vim.keymap.set("", "t", function()
+  --   require("hop").hint_char1({
+  --     direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+  --     -- current_line_only = true,
+  --     hint_offset = -1,
+  --   })
+  -- end, {})
+  --
+  -- vim.keymap.set("", "T", function()
+  --   require("hop").hint_char1({
+  --     direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+  --     -- current_line_only = true,
+  --     hint_offset = 1,
+  --   })
+  -- end, {})
 end
 
 return M
---
+++ config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+local M = {
+  event = "User PackerDefered",
+}
+
+function M.config()
+  require("mini.jump").setup({})
+end
+
+
+return M
