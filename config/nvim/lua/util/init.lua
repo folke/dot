1e0d3f69da7a5890dd90c4dfde5c49276938267c
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-02 16:12:17 +0200

    feat: dumpp uses lua as filetype instead of markdown

--- config/nvim/lua/util/init.lua
+++ config/nvim/lua/util/init.lua
@@ -5,12 +5,12 @@ end
 
 _G.dumpp = function(...)
   local msg = vim.inspect(...)
-  vim.notify("```lua\n" .. msg .. "\n```", vim.log.levels.INFO, {
+  vim.notify(msg, vim.log.levels.INFO, {
     title = "Debug",
     on_open = function(win)
       vim.api.nvim_win_set_option(win, "conceallevel", 3)
       local buf = vim.api.nvim_win_get_buf(win)
-      vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+      vim.api.nvim_buf_set_option(buf, "filetype", "lua")
       vim.api.nvim_win_set_option(win, "spell", false)
     end,
   })
