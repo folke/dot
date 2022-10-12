391d921cecd615e6a23ba927411431b0866e7750
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-12 08:30:14 +0200

    feat: better autocmd for showing/hiding cursorline in active window

--- config/nvim/lua/config/commands.lua
+++ config/nvim/lua/config/commands.lua
@@ -5,10 +5,24 @@ vim.cmd([[autocmd FileType markdown setlocal spell]])
 vim.cmd("au FocusGained * :checktime")
 
 -- show cursor line only in active window
-vim.cmd([[
-  autocmd InsertLeave,WinEnter * set cursorline
-  autocmd InsertEnter,WinLeave * set nocursorline
-]])
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  callback = function()
+    local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
+    if ok and cl then
+      vim.wo.cursorline = true
+      vim.api.nvim_win_del_var(0, "auto-cursorline")
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+  callback = function()
+    local cl = vim.wo.cursorline
+    if cl then
+      vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
+      vim.wo.cursorline = false
+    end
+  end,
+})
 
 -- create directories when needed, when saving a file
 vim.api.nvim_create_autocmd("BufWritePre", {
