49ee99ac5c4630e2145ce384d959d8ca10dd6187
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-22 20:04:07 +0200

    fix(fish): projects now uses gh to get my stuff

--- config/fish/functions/projects.fish
+++ config/fish/functions/projects.fish
@@ -1,8 +1,12 @@
-set myprojects which-key.nvim persistence.nvim workspace.nvim tokyonight.nvim trouble.nvim zen-mode.nvim lsp-colors.nvim todo-comments.nvim lua-dev.nvim twilight.nvim ultra-runner
+set -g myprojects (gh repo list --source --no-archived --json name --jq ".[].name")
 
 function projects
     for p in $myprojects
         set fp ~/projects/$p
+        # cd $fp
+        # # and git remote set-url origin https://github.com/$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')
+        # and git remote set-url origin git@github.com:$(    git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')
+        # and cd ..
         if test ! -d $fp
             echo "[clone] $fp"
             git clone git@github.com:folke/$p.git $fp
