19791ddd16463e312e1025d1bb2976cee0f06a2e
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-11-16 18:41:39 +0100

    feat(nvim): tokyonight updates

--- config/nvim/lua/plugins/tokyonight.lua
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -1,28 +1,33 @@
-return {
+local M = {
   opt = false,
-  config = function()
-    -- vim.o.background = "dark"
-    local tokyonight = require("tokyonight")
-    tokyonight.setup({
-      style = "moon",
-      -- transparent = true,
-      -- hide_inactive_statusline = false,
-      sidebars = {
-        "qf",
-        "vista_kind",
-        "terminal",
-        "packer",
-        "spectre_panel",
-        "NeogitStatus",
-        "help",
-        "startuptime",
-        "Outline",
-      },
-      on_colors = function(c) end,
-      on_highlights = function(hl, c)
-        -- make the current line cursor orange
-        hl.CursorLineNr = { fg = c.orange, bold = true }
+}
 
+function M.config()
+  -- vim.o.background = "dark"
+  local tokyonight = require("tokyonight")
+  tokyonight.setup({
+    style = "moon",
+    -- transparent = true,
+    -- hide_inactive_statusline = false,
+    sidebars = {
+      "qf",
+      "vista_kind",
+      "terminal",
+      -- "packer",
+      "spectre_panel",
+      "NeogitStatus",
+      -- "help",
+      "startuptime",
+      "Outline",
+    },
+    transparent = false,
+    styles = {},
+    on_colors = function(c) end,
+    on_highlights = function(hl, c)
+      -- make the current line cursor orange
+      hl.CursorLineNr = { fg = c.orange, bold = true }
+
+      if true then
         -- borderless telescope
         local prompt = "#2d3149"
         hl.TelescopeNormal = {
@@ -52,8 +57,10 @@ return {
           bg = c.bg_dark,
           fg = c.bg_dark,
         }
-      end,
-    })
-    tokyonight.load()
-  end,
-}
+      end
+    end,
+  })
+  tokyonight.load()
+end
+
+return M
