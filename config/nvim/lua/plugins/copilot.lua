ee51a219e8df5bd671c7fa2c076874b40759749b
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-11-16 17:54:55 +0100

    feat(ansible): added Twitter icon to brave app

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+local M = {
+  event = "User PackerDefered",
+}
+
+function M.config()
+  require("copilot").setup({})
+end
+
+function M.test() end
+
+return M
---
+++ config/nvim/lua/plugins/leap.lua
@@ -0,0 +1,19 @@
+local M = {
+  event = "User PackerDefered",
+  module = "leap",
+  requires = {
+    { "ggandor/flit.nvim", module = "flit" },
+    { "ggandor/leap-ast.nvim", module = "leap-ast" },
+  },
+}
+
+function M.config()
+  require("leap").add_default_mappings()
+  -- require("flit").setup()
+  -- require("leap").setup({})
+  vim.keymap.set({ "n", "x", "o" }, "M", function()
+    require("leap-ast").leap()
+  end, {})
+end
+
+return M
