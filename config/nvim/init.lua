5160dc571ec2fab975d21f14ffe32c14530be7c4
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-02 07:51:07 +0100

    feat: use lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,15 +1,40 @@
 local util = require("util")
 
--- util.debug_pcall()
+-- profile(function()
 
-require("util.dashboard").setup()
+-- util.debug_pcall()
+-- pcall(vim.cmd, "colorscheme tokyonight")
 
 util.require("config.options")
 
-vim.defer_fn(function()
-  util.require("config.commands")
-  util.version()
-  util.require("config.mappings")
-  util.packer_defered()
-  util.require("config.plugins")
-end, 100)
+vim.opt.runtimepath:append("~/projects/lazy.nvim/")
+
+local start = vim.loop.hrtime()
+-- load our colorscheme as soon as possible while installing
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = { "LazyPluginInstall", "LazyInstallPre" },
+--   callback = function(event)
+--     if event.match == "LazyInstallPre" or (event.data and event.data.plugin == "tokyonight") then
+--       vim.cmd("silent! colorscheme tokyonight")
+--     end
+--   end,
+-- })
+
+require("lazy").setup("config.plugins", {
+  defaults = { lazy = true },
+  dev = { patterns = { "folke" } },
+})
+
+local delta = vim.loop.hrtime() - start
+
+require("util.dashboard").setup()
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    vim.notify("Lazy took " .. (delta / 1e6) .. "ms")
+    util.require("config.commands")
+    util.version()
+    util.require("config.mappings")
+  end,
+})
