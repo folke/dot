b302328ce3fad11754862b8e06c15492c8d60573
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-18 10:02:56 +0100

    fix(doom): made it all work again

--- config/doom/init.el
+++ config/doom/init.el
@@ -35,7 +35,7 @@
        (emoji +github)                 ; Displays and inserts emojis (ASCII, Github style, unicode)
        ;;fill-column                   ; a `fill-column' indicator
        hl-todo                         ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
+       hydra
        indent-guides                   ; highlighted indent columns
        (ligatures +extra)              ; ligatures and symbols to make your code pretty again
        ;;minimap                       ; show a map of the code on the side
