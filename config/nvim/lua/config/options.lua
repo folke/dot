34fa91bb86ede49ab1ab4acfa67013ab342d2837
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-02 17:24:44 +0100

    refactor(nvim): options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -3,24 +3,26 @@ local indent = 2
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.node_host_prog = "/Users/folke/.pnpm-global/5/node_modules/neovim/bin/cli.js"
+
+-- vim.opt.concealcursor = "nc" -- Hide * markup for bold and italic
+vim.opt.backup = true
+
 vim.opt.autowrite = true -- enable auto write
 vim.opt.clipboard = "unnamedplus" -- sync with system clipboard
--- vim.opt.concealcursor = "nc" -- Hide * markup for bold and italic
+vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.conceallevel = 3 -- Hide * markup for bold and italic
 vim.opt.confirm = true -- confirm to save changes before exiting modified buffer
 vim.opt.cursorline = true -- Enable highlighting of the current line
 vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.backup = true
-vim.opt.spelllang = { "en" }
-
-vim.o.formatoptions = "jcroqlnt" -- tcqj
-vim.opt.guifont = "FiraCode Nerd Font:h11"
-vim.opt.grepprg = "rg --vimgrep"
+vim.opt.formatoptions = "jcroqlnt" -- tcqj
 vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.guifont = "FiraCode Nerd Font:h11"
 vim.opt.hidden = true -- Enable modified buffers in background
 vim.opt.ignorecase = true -- Ignore case
 vim.opt.inccommand = "nosplit" -- preview incremental substitute
 vim.opt.joinspaces = false -- No double spaces with join after a dot
+vim.opt.laststatus = 0
 vim.opt.list = true -- Show some invisible characters (tabs...
 vim.opt.mouse = "a" -- enable mouse mode
 vim.opt.number = true -- Print line number
@@ -28,15 +30,15 @@ vim.opt.pumblend = 10 -- Popup blend
 vim.opt.pumheight = 10 -- Maximum number of entries in a popup
 vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 vim.opt.shiftround = true -- Round indent
 vim.opt.shiftwidth = indent -- Size of an indent
-vim.opt.laststatus = 0
 vim.opt.showmode = false -- dont show mode since we have a statusline
 vim.opt.sidescrolloff = 8 -- Columns of context
 vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 vim.opt.smartcase = true -- Don't ignore case with capitals
 vim.opt.smartindent = true -- Insert indents automatically
-
+vim.opt.spelllang = { "en" }
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.tabstop = indent -- Number of spaces tabs count for
@@ -45,22 +47,7 @@ vim.opt.undofile = true
 vim.opt.undolevels = 10000
 vim.opt.updatetime = 200 -- save swap file and trigger CursorHold
 vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
-vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.wrap = false -- Disable line wrap
-vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
-vim.opt.fillchars = {
-  --   horiz = "━",
-  --   horizup = "┻",
-  --   horizdown = "┳",
-  --   vert = "┃",
-  --   vertleft = "┫",
-  --   vertright = "┣",
-  --   verthoriz = "╋",im.o.fillchars = [[eob: ,
-  -- fold = " ",
-  foldopen = "",
-  -- foldsep = " ",
-  foldclose = "",
-}
 vim.g.markdown_recommended_style = 0
 
 if vim.fn.has("nvim-0.8") == 1 then
