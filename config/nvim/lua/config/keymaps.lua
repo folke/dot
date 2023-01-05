81bf012fbb1cddc2a0e1ea7df7e47be49b4c0b6e
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-05 11:29:38 +0100

    refactor: moved cowboy mode to util

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,26 +1,6 @@
 local util = require("util")
 
-local id
-for _, key in ipairs({ "h", "j", "k", "l" }) do
-  local count = 0
-  vim.keymap.set("n", key, function()
-    if count >= 10 then
-      id = vim.notify("Hold it Cowboy!", vim.log.levels.WARN, {
-        icon = "🤠",
-        replace = id,
-        keep = function()
-          return count >= 10
-        end,
-      })
-    else
-      count = count + 1
-      vim.defer_fn(function()
-        count = count - 1
-      end, 5000)
-      return key
-    end
-  end, { expr = true })
-end
+util.cowboy()
 
 -- Move to window using the movement keys
 vim.keymap.set("n", "<left>", "<C-w>h")
--- config/nvim/lua/util/init.lua
+++ config/nvim/lua/util/init.lua
@@ -166,4 +166,29 @@ function M.version()
   end
 end
 
+function M.cowboy()
+  ---@type table?
+  local id
+  for _, key in ipairs({ "h", "j", "k", "l" }) do
+    local count = 0
+    vim.keymap.set("n", key, function()
+      if count >= 10 then
+        id = vim.notify("Hold it Cowboy!", vim.log.levels.WARN, {
+          icon = "🤠",
+          replace = id,
+          keep = function()
+            return count >= 10
+          end,
+        })
+      else
+        count = count + 1
+        vim.defer_fn(function()
+          count = count - 1
+        end, 5000)
+        return key
+      end
+    end, { expr = true, silent = true })
+  end
+end
+
 return M
