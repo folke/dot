f455f8fe58dfd47deff6b7b44979ccf59343a867
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-10 23:03:18 +0100

    fix(yanky): keymaps

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -122,18 +122,19 @@ return {
       vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
       vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
 
-      -- vim.keymap.set("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)")
-      -- vim.keymap.set("n", "]P", "<Plug>(YankyPutIndentAfterLinewise)")
-      -- vim.keymap.set("n", "[p", "<Plug>(YankyPutIndentBeforeLinewise)")
-      -- vim.keymap.set("n", "[P", "<Plug>(YankyPutIndentBeforeLinewise)")
-      --
-      -- vim.keymap.set("n", ">p", "<Plug>(YankyPutIndentAfterShiftRight)")
-      -- vim.keymap.set("n", "<p", "<Plug>(YankyPutIndentAfterShiftLeft)")
-      -- vim.keymap.set("n", ">P", "<Plug>(YankyPutIndentBeforeShiftRight)")
-      -- vim.keymap.set("n", "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)")
+      vim.keymap.set("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)")
+      vim.keymap.set("n", "[p", "<Plug>(YankyPutIndentBeforeLinewise)")
+      vim.keymap.set("n", "]P", "<Plug>(YankyPutIndentAfterLinewise)")
+      vim.keymap.set("n", "[P", "<Plug>(YankyPutIndentBeforeLinewise)")
 
-      vim.keymap.set("n", "]p", "<Plug>(YankyPutAfterFilter)")
-      vim.keymap.set("n", "[p", "<Plug>(YankyPutBeforeFilter)")
+      vim.keymap.set("n", ">p", "<Plug>(YankyPutIndentAfterShiftRight)")
+      vim.keymap.set("n", "<p", "<Plug>(YankyPutIndentAfterShiftLeft)")
+      vim.keymap.set("n", ">P", "<Plug>(YankyPutIndentBeforeShiftRight)")
+      vim.keymap.set("n", "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)")
+
+      vim.keymap.set("n", "=p", "<Plug>(YankyPutAfterFilter)")
+      vim.keymap.set("n", "=P", "<Plug>(YankyPutBeforeFilter)")
+
       vim.keymap.set("n", "<leader>P", function()
         require("telescope").extensions.yank_history.yank_history({})
       end, { desc = "Paste from Yanky" })
