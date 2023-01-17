7602d8a36d85caa2cd4bd43f1bcf1c27f120a4c5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-17 17:29:22 +0100

    feat: I moved to Arch btw

--- config/fish/conf.d/brew.fish
+++
@@ -1,8 +0,0 @@
-set -l prefix "/home/linuxbrew/.linuxbrew"
-
-set -gx HOMEBREW_PREFIX $prefix
-set -gx HOMEBREW_CELLAR $prefix/Cellar
-set -gx HOMEBREW_REPOSITORY $prefix/Homebrew
-set -gx HOMEBREW_SHELLENV_PREFIX $prefix
-
-set -gx PATH $PATH $prefix/bin $prefix/sbin
