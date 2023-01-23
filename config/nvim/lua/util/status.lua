b8cca156b4169b1ce7e15302a19fc509ec0e157f
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-23 20:57:19 +0100

    feat(nvim): better statuscolumn

--- config/nvim/lua/util/status.lua
+++ config/nvim/lua/util/status.lua
@@ -1,29 +1,38 @@
 local M = {}
 _G.Status = M
 
-function M.fold()
-  local line = vim.v.lnum
-  -- if vim.fn.foldlevel(line) > 1 then
-  -- 	return " "
-  -- end
-
-  if vim.fn.foldlevel(line) <= vim.fn.foldlevel(line - 1) then
-    return " "
-  end
-
-  return vim.fn.foldclosed(line) > 0 and "" or ""
+---@return {name:string, text:string, texthl:string}[]
+function M.get_signs()
+  local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  return vim.tbl_map(function(sign)
+    return vim.fn.sign_getdefined(sign.name)[1]
+  end, vim.fn.sign_getplaced(buf, { group = "*", lnum = vim.v.lnum })[1].signs)
 end
 
-function M.status_column()
+function M.column()
+  local sign, git_sign
+  for _, s in ipairs(M.get_signs()) do
+    if s.name:find("GitSign") then
+      git_sign = s
+    else
+      sign = s
+    end
+  end
+
+  local nu = " "
+  local number = vim.api.nvim_win_get_option(vim.g.statusline_winid, "number")
+  if number and vim.wo.relativenumber and vim.v.virtnum == 0 then
+    nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+  end
   local components = {
+    sign and ("%#" .. sign.texthl .. "#" .. sign.text .. "%*") or " ",
     [[%=]],
-    [[%{&nu?(&rnu&&v:relnum?v:relnum:v:lnum):''}]],
-    [[ %s]],
-    -- [[%#FoldColumn#%{v:lua.Status.fold()} ]],
+    nu .. " ",
+    git_sign and ("%#" .. git_sign.texthl .. "#" .. git_sign.text .. "%*") or "  ",
   }
   return table.concat(components, "")
 end
 
-vim.opt.statuscolumn = M.status_column()
+vim.opt.statuscolumn = [[%!v:lua.Status.column()]]
 
 return M
