d5013a1c1f7f4e96b0d16e1c425941e39572271f
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-24 19:55:00 +0100

    refactor: simplified telescope setup

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,31 +1,31 @@
 return {
-  "telescope.nvim",
-  dependencies = {
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  keys = {
-    -- { "<leader>/", vim.NIL },
-    {
-      "<leader>fp",
-      function()
-        require("telescope.builtin").find_files({
-          cwd = require("lazy.core.config").options.root,
-        })
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
       end,
-      desc = "Find Plugin File",
+    },
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({
+            cwd = require("lazy.core.config").options.root,
+          })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
     },
   },
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      layout_config = { prompt_position = "top" },
-      sorting_strategy = "ascending",
-      winblend = 0,
-    },
-  },
-  config = function(_, opts)
-    local telescope = require("telescope")
-    telescope.setup(opts)
-    telescope.load_extension("fzf")
-  end,
 }
