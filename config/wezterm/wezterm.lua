2c6aa85712d8481c937129b0e16deb5f50dc1166
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-27 18:19:21 +0100

    feat: updated dots for hyprland and moved to wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,23 +1,182 @@
-local wt = require("wezterm")
+local wezterm = require("wezterm")
+local act = wezterm.action
+local mod = "SHIFT|SUPER"
+
+local function font(opts)
+  return wezterm.font_with_fallback({
+    opts,
+    "Symbols Nerd Font Mono",
+  })
+end
+
+function make_mouse_binding(dir, streak, button, mods, action)
+  return {
+    event = { [dir] = { streak = streak, button = button } },
+    mods = mods,
+    action = action,
+  }
+end
 
 return {
-	font_size = 12,
-	font = wt.font("FiraCode Nerd Font"),
-	font_rules = {
-		{
-			intensity = "Bold",
-			font = wt.font("FiraCode Nerd Font", { weight = "Bold" }),
-		},
-		{
-			italic = true,
-			font = wt.font("VictorMono Nerd Font", { weight = "Light", style = "Italic" }),
-		},
-		{
-			italic = true,
-			intensity = "Bold",
-			font = wt.font("VictorMono Nerd Font", { weight = "Bold", style = "Italic" }),
-		},
+  -- stylua: ignore
+	mouse_bindings = {
+		make_mouse_binding( "Up", 1, "Left", "NONE", wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("ClipboardAndPrimarySelection")),
+		make_mouse_binding( "Up", 1, "Left", "SHIFT", wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("ClipboardAndPrimarySelection")),
+		make_mouse_binding("Up", 1, "Left", "ALT", wezterm.action.CompleteSelection("ClipboardAndPrimarySelection")),
+		make_mouse_binding( "Up", 1, "Left", "SHIFT|ALT", wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("ClipboardAndPrimarySelection")),
+		make_mouse_binding("Up", 2, "Left", "NONE", wezterm.action.CompleteSelection("ClipboardAndPrimarySelection")),
+		make_mouse_binding("Up", 3, "Left", "NONE", wezterm.action.CompleteSelection("ClipboardAndPrimarySelection")),
 	},
-	color_scheme_dirs = { "/home/folke/projects/tokyonight.nvim/extras/wezterm" },
-	color_scheme = "tokyonight_storm",
+  term = "wezterm",
+  font_size = 12,
+  font = font("Fira Code"),
+  font_rules = {
+    {
+      italic = true,
+      intensity = "Normal",
+      font = font({
+        family = "Victor Mono",
+        style = "Italic",
+      }),
+    },
+    {
+      italic = true,
+      intensity = "Half",
+      font = font({
+        family = "Victor Mono",
+        weight = "DemiBold",
+        style = "Italic",
+      }),
+    },
+    {
+      italic = true,
+      intensity = "Bold",
+      font = font({
+        family = "Victor Mono",
+        weight = "Bold",
+        style = "Italic",
+      }),
+    },
+  },
+  color_scheme_dirs = { "/home/folke/projects/tokyonight.nvim/extras/wezterm" },
+  color_scheme = "tokyonight_moon",
+  use_fancy_tab_bar = true,
+  tab_bar_at_bottom = true,
+  hide_tab_bar_if_only_one_tab = true,
+  show_tab_index_in_tab_bar = false,
+  -- window_decorations = "NONE",
+  window_frame = {
+    -- The font used in the tab bar.
+    -- Roboto Bold is the default; this font is bundled
+    -- with wezterm.
+    -- Whatever font is selected here, it will have the
+    -- main font setting appended to it to pick up any
+    -- fallback fonts you may have used there.
+    font = font({ family = "Fira Code", weight = "Bold" }),
+
+    -- The size of the font in the tab bar.
+    -- Default to 10. on Windows but 12.0 on other systems
+    font_size = 11.0,
+
+    -- The overall background color of the tab bar when
+    -- the window is focused
+    active_titlebar_bg = "#191b28",
+
+    -- The overall background color of the tab bar when
+    -- the window is not focused
+    inactive_titlebar_bg = "#1e2030",
+  },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  colors = {
+    tab_bar = {
+      -- The color of the inactive tab bar edge/divider
+      inactive_tab_edge = "#1e2030",
+      background = "#191b28",
+      active_tab = {
+        fg_color = "#82aaff",
+        bg_color = "#222436",
+      },
+      inactive_tab = {
+        bg_color = "#1e2030",
+        fg_color = "#545c7e",
+      },
+      inactive_tab_hover = {
+        bg_color = "#1e2030",
+        fg_color = "#82aaff",
+      },
+      new_tab_hover = {
+        fg_color = "#1e2030",
+        bg_color = "#82aaff",
+      },
+      new_tab = {
+        fg_color = "#82aaff",
+        bg_color = "#191b28",
+      },
+    },
+  },
+  disable_default_key_bindings = true,
+  keys = {
+    { mods = mod, key = "UpArrow", action = act.ActivatePaneDirection("Up") },
+    { mods = mod, key = "DownArrow", action = act.ActivatePaneDirection("Down") },
+    { mods = mod, key = "RightArrow", action = act.ActivatePaneDirection("Right") },
+    { mods = mod, key = "LeftArrow", action = act.ActivatePaneDirection("Left") },
+    { mods = mod, key = "t", action = act.SpawnTab("CurrentPaneDomain") },
+    { mods = mod, key = "|", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { mods = mod, key = "_", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { mods = mod, key = ">", action = act.MoveTabRelative(1) },
+    { mods = mod, key = "<", action = act.MoveTabRelative(-1) },
+    { mods = mod, key = "M", action = act.TogglePaneZoomState },
+    { mods = mod, key = "p", action = act.PaneSelect({ alphabet = "", mode = "Activate" }) },
+    { mods = mod, key = "C", action = act.CopyTo("ClipboardAndPrimarySelection") },
+    { mods = mod, key = "l", action = wezterm.action({ ActivateTabRelative = 1 }) },
+    { mods = mod, key = "h", action = wezterm.action({ ActivateTabRelative = -1 }) },
+    { key = "C", mods = "CTRL", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
+  },
+  bold_brightens_ansi_colors = true,
+  window_background_opacity = 0.9,
+  cell_width = 0.9,
+  scrollback_lines = 10000,
+  hyperlink_rules = {
+    -- Linkify things that look like URLs and the host has a TLD name.
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+      format = "$0",
+    },
+
+    -- linkify email addresses
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+      format = "mailto:$0",
+    },
+
+    -- file:// URI
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = [[\bfile://\S*\b]],
+      format = "$0",
+    },
+
+    -- Linkify things that look like URLs with numeric addresses as hosts.
+    -- E.g. http://127.0.0.1:8000 for a local development server,
+    -- or http://192.168.1.1 for the web interface of many routers.
+    {
+      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+      format = "$0",
+    },
+
+    -- Make username/project paths clickable. This implies paths like the following are for GitHub.
+    -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+    -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+    {
+      regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+      format = "https://www.github.com/$1/$3",
+    },
+  },
 }
