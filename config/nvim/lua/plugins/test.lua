9cc38a27c7835b491f544d5d61a58234759dfc7a
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-28 11:57:32 +0100

    chore: updates

---
+++ config/nvim/lua/plugins/test.lua
@@ -0,0 +1,55 @@
+-- if true then
+--   return {}
+-- end
+return {
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   event = "VeryLazy",
+  --   opts = function()
+  --     return {
+  --       options = {
+  --         always_show_bufferline = true,
+  --       },
+  --     }
+  --   end,
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "rcarriga/nvim-notify",
+  --   opts = {
+  --     background_colour = "#000000", -- doesn't work
+  --   },
+  -- },
+  -- { "stevearc/dressing.nvim", enabled = false },
+  -- { "rcarriga/nvim-notify", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
+  -- { "akinsho/bufferline.nvim", enabled = false },
+  --
+  -- {
+  --   "romgrk/barbar.nvim",
+  --   event = "VeryLazy",
+  --   config = true,
+  -- },
+  -- { "ggandor/flit.nvim", enabled = false },
+  -- {
+  --   "unblevable/quick-scope",
+  --   lazy = false,
+  --   init = function()
+  --     vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
+  --   end,
+  -- },
+  --
+  -- {
+  --   "wookayin/semshi",
+  --   build = ":UpdateRemotePlugins",
+  -- },
+}
