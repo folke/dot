6c08526d7478ff0b2aa0ab89c455f85524300894
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-20 09:20:05 +0100

    feat(nvim): testing bracketed

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -92,16 +92,61 @@ return {
     opts = {},
   },
 
+  {
+    "echasnovski/nvim",
+    event = "BufReadPost",
+    name = "bracketed",
+    submodules = false,
+    config = function()
+      local bracketed = require("mini-dev.bracketed")
+
+      local function put(cmd, regtype)
+        local body = vim.fn.getreg(vim.v.register)
+        local type = vim.fn.getregtype(vim.v.register)
+        ---@diagnostic disable-next-line: param-type-mismatch
+        vim.fn.setreg(vim.v.register, body, regtype or "l")
+        bracketed.register_put_region()
+        vim.cmd(('normal! "%s%s'):format(vim.v.register, cmd:lower()))
+        ---@diagnostic disable-next-line: param-type-mismatch
+        vim.fn.setreg(vim.v.register, body, type)
+      end
+
+      for _, cmd in ipairs({ "]p", "[p" }) do
+        vim.keymap.set("n", cmd, function()
+          put(cmd)
+        end)
+      end
+      for _, cmd in ipairs({ "]P", "[P" }) do
+        vim.keymap.set("n", cmd, function()
+          put(cmd, "c")
+        end)
+      end
+
+      local put_keys = { "p", "P" }
+      for _, lhs in ipairs(put_keys) do
+        vim.keymap.set({ "n", "x" }, lhs, function()
+          return bracketed.register_put_region(lhs)
+        end, { expr = true })
+      end
+
+      bracketed.setup({
+        file = { suffix = "" },
+        window = { suffix = "" },
+        treesitter = { suffix = "n" },
+      })
+    end,
+  },
+
   -- better yank/paste
   {
     "kkharji/sqlite.lua",
     enabled = function()
-      return not jit.os:find("Windows")
+      return require("lazyvim.util").has("yanky.nvim") and not jit.os:find("Windows")
     end,
   },
   {
     "gbprod/yanky.nvim",
-    enabled = true,
+    enabled = false,
     event = "BufReadPost",
     config = function()
       -- vim.g.clipboard = {
