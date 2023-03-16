1a43d06b565bcc003f5959c98f3175e419f968db
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-16 12:41:02 +0100

    feat(init.lua): load config.env

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,7 @@ require("config.lazy")({
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
+    require("config.env")
     require("util").version()
   end,
 })
