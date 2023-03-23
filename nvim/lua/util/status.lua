216ba261d533c9eb487dab13130cf2041a11598a
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-23 12:38:52 +0100

    fix(status): fixed statusline implementation

--- nvim/lua/util/status.lua
+++ nvim/lua/util/status.lua
@@ -2,16 +2,17 @@ local M = {}
 _G.Status = M
 
 ---@return {name:string, text:string, texthl:string}[]
-function M.get_signs()
-  local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+function M.get_signs(win)
+  local buf = vim.api.nvim_win_get_buf(win)
   return vim.tbl_map(function(sign)
     return vim.fn.sign_getdefined(sign.name)[1]
   end, vim.fn.sign_getplaced(buf, { group = "*", lnum = vim.v.lnum })[1].signs)
 end
 
 function M.column()
+  local win = vim.g.statusline_winid
   local sign, git_sign
-  for _, s in ipairs(M.get_signs()) do
+  for _, s in ipairs(M.get_signs(win)) do
     if s.name:find("GitSign") then
       git_sign = s
     else
@@ -20,8 +21,7 @@ function M.column()
   end
 
   local nu = " "
-  local number = vim.api.nvim_win_get_option(vim.g.statusline_winid, "number")
-  if number and vim.wo.relativenumber and vim.v.virtnum == 0 then
+  if vim.wo[win].number and vim.wo[win].relativenumber and vim.v.virtnum == 0 then
     nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
   end
   local components = {
