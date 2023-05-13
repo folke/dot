19514bb90c987f458591ac90d3301002f90b14b0
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-13 16:36:14 +0200

    feat(noice): base noice config off the LazyVim one and upstreamed written notifs

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,77 +1,43 @@
-local M = {
+return {
   "folke/noice.nvim",
-  event = "VeryLazy",
-}
+  opts = function(_, opts)
+    local focused = true
+    vim.api.nvim_create_autocmd("FocusGained", {
+      callback = function()
+        focused = true
+      end,
+    })
+    vim.api.nvim_create_autocmd("FocusLost", {
+      callback = function()
+        focused = false
+      end,
+    })
+    table.insert(opts.routes, 1, {
+      filter = {
+        cond = function()
+          return not focused
+        end,
+      },
+      view = "notify_send",
+      opts = { stop = false },
+    })
 
-function M.config()
-  local focused = true
-  vim.api.nvim_create_autocmd("FocusGained", {
-    callback = function()
-      focused = true
-    end,
-  })
-  vim.api.nvim_create_autocmd("FocusLost", {
-    callback = function()
-      focused = false
-    end,
-  })
-  require("noice").setup({
-    debug = false,
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
-    },
-    routes = {
-      {
-        filter = {
-          cond = function()
-            return not focused
-          end,
-        },
-        view = "notify_send",
-        opts = { stop = false },
-      },
-      {
-        filter = {
-          event = "msg_show",
-          find = "%d+L, %d+B",
-        },
-        view = "mini",
-      },
-    },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
-      inc_rename = true,
-      cmdline_output_to_split = false,
-    },
-    commands = {
+    opts.commands = {
       all = {
         -- options for the message history that you get with `:Noice`
         view = "split",
         opts = { enter = true, format = "details" },
         filter = {},
       },
-    },
-    format = {
-      level = {
-        icons = false,
-      },
-    },
-  })
+    }
 
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = function(event)
-      vim.schedule(function()
-        require("noice.text.markdown").keys(event.buf)
-      end)
-    end,
-  })
-end
-
-return M
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function(event)
+        vim.schedule(function()
+          require("noice.text.markdown").keys(event.buf)
+        end)
+      end,
+    })
+  end,
+}
