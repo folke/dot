4d6259b0a74f6347a2a3758751dc1131d146bf9d
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-13 16:25:33 +0200

    perf: defer matchparen

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -40,6 +40,7 @@ return {
     "andymass/vim-matchup",
     event = "BufReadPost",
     config = function()
+      vim.g.matchup_matchparen_deferred = 1
       vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
     end,
   },
