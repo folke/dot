aab142fc54e6cd7ebf971ca8a4f4af7209ed197a
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-13 16:35:18 +0200

    feat(matchup): added `<>` to matchpairs

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -39,6 +39,9 @@ return {
   {
     "andymass/vim-matchup",
     event = "BufReadPost",
+    init = function()
+      vim.o.matchpairs = "(:),{:},[:],<:>"
+    end,
     config = function()
       vim.g.matchup_matchparen_deferred = 1
       vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
