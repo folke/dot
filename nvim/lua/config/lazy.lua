64620668067285e03f4170c31efb1cda9c4a3e0a
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-13 16:35:52 +0200

    feat(lazy): added tailwind extra

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ return function(opts)
       { import = "lazyvim.plugins.extras.formatting.prettier" },
       { import = "lazyvim.plugins.extras.coding.copilot" },
       { import = "lazyvim.plugins.extras.lang.json" },
+      { import = "lazyvim.plugins.extras.lang.tailwind" },
       { import = "lazyvim.plugins.extras.ui.mini-animate" },
       { import = "lazyvim.plugins.extras.dap.core" },
       { import = "lazyvim.plugins.extras.dap.nlua" },
