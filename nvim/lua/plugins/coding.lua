e94f48b8ce3209ab2e41abce0047858f451f5179
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-13 16:25:04 +0200

    feat(nvim): testing starcoder

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,4 +1,19 @@
 return {
+  {
+    "huggingface/hfcc.nvim",
+    opts = {
+      api_token = "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",
+      model = "bigcode/starcoder",
+      query_params = {
+        max_new_tokens = 200,
+      },
+    },
+    init = function()
+      vim.api.nvim_create_user_command("StarCoder", function()
+        require("hfcc.completion").complete()
+      end, {})
+    end,
+  },
   -- better text objects
   {
     "echasnovski/mini.ai",
