ee38d35f5af8795b40989e92f52ea56f4dd8e571
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-13 16:24:51 +0200

    feat(nvim): use yanky again. Missed the auto synching between clipboard and neovim too much

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -98,39 +98,40 @@ return {
     config = function()
       local bracketed = require("mini.bracketed")
 
-      local function put(cmd, regtype)
-        local body = vim.fn.getreg(vim.v.register)
-        local type = vim.fn.getregtype(vim.v.register)
-        ---@diagnostic disable-next-line: param-type-mismatch
-        vim.fn.setreg(vim.v.register, body, regtype or "l")
-        bracketed.register_put_region()
-        vim.cmd(('normal! "%s%s'):format(vim.v.register, cmd:lower()))
-        ---@diagnostic disable-next-line: param-type-mismatch
-        vim.fn.setreg(vim.v.register, body, type)
-      end
-
-      for _, cmd in ipairs({ "]p", "[p" }) do
-        vim.keymap.set("n", cmd, function()
-          put(cmd)
-        end)
-      end
-      for _, cmd in ipairs({ "]P", "[P" }) do
-        vim.keymap.set("n", cmd, function()
-          put(cmd, "c")
-        end)
-      end
-
-      local put_keys = { "p", "P" }
-      for _, lhs in ipairs(put_keys) do
-        vim.keymap.set({ "n", "x" }, lhs, function()
-          return bracketed.register_put_region(lhs)
-        end, { expr = true })
-      end
+      -- local function put(cmd, regtype)
+      --   local body = vim.fn.getreg(vim.v.register)
+      --   local type = vim.fn.getregtype(vim.v.register)
+      --   ---@diagnostic disable-next-line: param-type-mismatch
+      --   vim.fn.setreg(vim.v.register, body, regtype or "l")
+      --   bracketed.register_put_region()
+      --   vim.cmd(('normal! "%s%s'):format(vim.v.register, cmd:lower()))
+      --   ---@diagnostic disable-next-line: param-type-mismatch
+      --   vim.fn.setreg(vim.v.register, body, type)
+      -- end
+      --
+      -- for _, cmd in ipairs({ "]p", "[p" }) do
+      --   vim.keymap.set("n", cmd, function()
+      --     put(cmd)
+      --   end)
+      -- end
+      -- for _, cmd in ipairs({ "]P", "[P" }) do
+      --   vim.keymap.set("n", cmd, function()
+      --     put(cmd, "c")
+      --   end)
+      -- end
+      --
+      -- local put_keys = { "p", "P" }
+      -- for _, lhs in ipairs(put_keys) do
+      --   vim.keymap.set({ "n", "x" }, lhs, function()
+      --     return bracketed.register_put_region(lhs)
+      --   end, { expr = true })
+      -- end
 
       bracketed.setup({
         file = { suffix = "" },
         window = { suffix = "" },
         quickfix = { suffix = "" },
+        yank = { suffix = "" },
         treesitter = { suffix = "n" },
       })
     end,
@@ -138,65 +139,35 @@ return {
 
   -- better yank/paste
   {
-    "kkharji/sqlite.lua",
-    enabled = function()
-      return require("lazyvim.util").has("yanky.nvim") and not jit.os:find("Windows")
-    end,
-  },
-  {
     "gbprod/yanky.nvim",
-    enabled = false,
-    event = "BufReadPost",
-    config = function()
-      -- vim.g.clipboard = {
-      --   name = "xsel_override",
-      --   copy = {
-      --     ["+"] = "xsel --input --clipboard",
-      --     ["*"] = "xsel --input --primary",
-      --   },
-      --   paste = {
-      --     ["+"] = "xsel --output --clipboard",
-      --     ["*"] = "xsel --output --primary",
-      --   },
-      --   cache_enabled = 1,
-      -- }
-
-      require("yanky").setup({
-        highlight = {
-          timer = 150,
-        },
-        ring = {
-          storage = jit.os:find("Windows") and "shada" or "sqlite",
-        },
-      })
-
-      vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
-
-      vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-      vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-      vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
-      vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
-
-      vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
-      vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
-
-      vim.keymap.set("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)")
-      vim.keymap.set("n", "[p", "<Plug>(YankyPutIndentBeforeLinewise)")
-      vim.keymap.set("n", "]P", "<Plug>(YankyPutIndentAfterLinewise)")
-      vim.keymap.set("n", "[P", "<Plug>(YankyPutIndentBeforeLinewise)")
-
-      vim.keymap.set("n", ">p", "<Plug>(YankyPutIndentAfterShiftRight)")
-      vim.keymap.set("n", "<p", "<Plug>(YankyPutIndentAfterShiftLeft)")
-      vim.keymap.set("n", ">P", "<Plug>(YankyPutIndentBeforeShiftRight)")
-      vim.keymap.set("n", "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)")
-
-      vim.keymap.set("n", "=p", "<Plug>(YankyPutAfterFilter)")
-      vim.keymap.set("n", "=P", "<Plug>(YankyPutBeforeFilter)")
-
-      vim.keymap.set("n", "<leader>P", function()
-        require("telescope").extensions.yank_history.yank_history({})
-      end, { desc = "Paste from Yanky" })
-    end,
+    enabled = true,
+    event = "VeryLazy",
+    dependencies = { { "kkharji/sqlite.lua", enabled = not jit.os:find("Windows") } },
+    opts = {
+      highlight = { timer = 150 },
+      ring = { storage = jit.os:find("Windows") and "shada" or "sqlite" },
+    },
+    keys = {
+      -- stylua: ignore
+      { "<leader>P", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Paste from Yanky" },
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" } },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
+      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" } },
+      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" } },
+      { "[y", "<Plug>(YankyCycleForward)" },
+      { "]y", "<Plug>(YankyCycleBackward)" },
+      { "]p", "<Plug>(YankyPutIndentAfterLinewise)" },
+      { "[p", "<Plug>(YankyPutIndentBeforeLinewise)" },
+      { "]P", "<Plug>(YankyPutIndentAfterLinewise)" },
+      { "[P", "<Plug>(YankyPutIndentBeforeLinewise)" },
+      { ">p", "<Plug>(YankyPutIndentAfterShiftRight)" },
+      { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)" },
+      { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)" },
+      { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)" },
+      { "=p", "<Plug>(YankyPutAfterFilter)" },
+      { "=P", "<Plug>(YankyPutBeforeFilter)" },
+    },
   },
 
   -- better increase/descrease
@@ -216,6 +187,7 @@ return {
           augend.date.alias["%Y/%m/%d"],
           augend.constant.alias.bool,
           augend.semver.alias.semver,
+          augend.constant.new({ elements = { "let", "const" } }),
         },
       })
     end,
