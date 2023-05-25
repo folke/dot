d6132a9cecd7919f5b3a22c5bdaff4444b0157d2
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-25 22:16:42 +0200

    chore: disabled neorg and removed nvim-colorizer

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -5,6 +5,7 @@ return {
   -- neorg
   {
     "nvim-neorg/neorg",
+    enabled = false,
     ft = "norg",
     opts = {
       load = {
@@ -47,30 +48,6 @@ return {
     keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
   },
 
-  -- colorizer
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = {
-      filetypes = { "*", "!lazy" },
-      buftype = { "*", "!prompt", "!nofile" },
-      user_default_options = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = false, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        AARRGGBB = false, -- 0xAARRGGBB hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes: foreground, background
-        -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = "background", -- Set the display mode.
-        virtualtext = "■",
-      },
-    },
-  },
   {
     "jackMort/ChatGPT.nvim",
     cmd = { "ChatGPTActAs", "ChatGPT" },
