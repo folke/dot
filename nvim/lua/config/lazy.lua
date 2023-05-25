b9351731b737ceb50249e465542f495b76bc46d7
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-25 22:40:22 +0200

    refactor: use the new lazyvim extra for mini.hipatterns

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ return function(opts)
       { import = "lazyvim.plugins.extras.dap.core" },
       { import = "lazyvim.plugins.extras.vscode" },
       { import = "lazyvim.plugins.extras.dap.nlua" },
+      { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
       -- { import = "lazyvim.plugins.extras.util.project" },
       { import = "plugins" },
     },
