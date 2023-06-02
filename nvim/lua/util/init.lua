28425f163a965f73a577206bc9af9e93ee4e04e0
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-02 17:24:57 +0200

    fix: better cowboy mode

--- nvim/lua/util/init.lua
+++ nvim/lua/util/init.lua
@@ -125,9 +125,13 @@ end
 function M.cowboy()
   ---@type table?
   local id
-  for _, key in ipairs({ "h", "j", "k", "l" }) do
+  for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
     local count = 0
+    local timer = assert(vim.loop.new_timer())
     vim.keymap.set("n", key, function()
+      if vim.v.count > 0 then
+        count = 0
+      end
       if count >= 10 then
         id = vim.notify("Hold it Cowboy!", vim.log.levels.WARN, {
           icon = "🤠",
@@ -138,9 +142,9 @@ function M.cowboy()
         })
       else
         count = count + 1
-        vim.defer_fn(function()
-          count = count - 1
-        end, 5000)
+        timer:start(2000, 0, function()
+          count = 0
+        end)
         return key
       end
     end, { expr = true, silent = true })
