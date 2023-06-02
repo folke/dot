799ddedfb005a9a35fc9e3b7834ff5eba9010c73
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-02 17:24:24 +0200

    feat: some experiments

---
+++ nvim/lua/util/stuff/class_conceal.lua
@@ -0,0 +1,40 @@
+local conceal_ns = vim.api.nvim_create_namespace("class_conceal")
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged", "InsertLeave" }, {
+  group = vim.api.nvim_create_augroup("class_conceal", { clear = true }),
+  pattern = { "*.tsx" },
+  callback = function(event)
+    do
+      return
+    end
+    dd(event)
+    local bufnr = event.buf or vim.api.nvim_get_current_buf()
+
+    ---Conceal HTML class attributes. Ideal for big TailwindCSS class lists
+    ---Ref: https://gist.github.com/mactep/430449fd4f6365474bfa15df5c02d27b
+    local language_tree = vim.treesitter.get_parser(bufnr, "tsx")
+    local syntax_tree = language_tree:parse()
+    local root = syntax_tree[1]:root()
+
+    local ok, query = pcall(
+      vim.treesitter.query.parse,
+      "tsx",
+      [[
+    ((jsx_attribute
+        (property_identifier) @att_name (#eq? @att_name "class")
+        (string (string_fragment) @class_value)))
+    ]]
+    )
+    if not ok then
+      dd(query)
+    end
+
+    for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_(), {}) do
+      local start_row, start_col, end_row, end_col = captures[2]:range()
+      vim.api.nvim_buf_set_extmark(bufnr, conceal_ns, start_row, start_col + 3, {
+        end_line = end_row,
+        end_col = end_col,
+        conceal = "%", -- "…",
+      })
+    end
+  end,
+})
---
+++ nvim/lua/util/stuff/colors.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+M.ns = vim.api.nvim_create_namespace("lsp.colors")
+
+---@type table<string,true>
+M.hl = {}
+
+---@param hex string
+function M.get_hl(hex)
+  local hl = "LspColor" .. hex:sub(2)
+  if not M.hl[hl] then
+    M.hl[hl] = true
+    vim.api.nvim_set_hl(0, hl, { bg = hex })
+  end
+  return hl
+end
+
+function M.update(buf)
+  buf = buf or vim.api.nvim_get_current_buf()
+  local params = { textDocument = vim.lsp.util.make_text_document_params(buf) }
+  vim.lsp.buf_request(buf, "textDocument/documentColor", params, function(err, colors)
+    if err then
+      return
+    end
+    for _, c in ipairs(colors) do
+      local color = c.color
+      color.red = math.floor(color.red * 255 + 0.5)
+      color.green = math.floor(color.green * 255 + 0.5)
+      color.blue = math.floor(color.blue * 255 + 0.5)
+      local hex = string.format("#%02x%02x%02x", color.red, color.green, color.blue)
+
+      local offset_encoding = vim.lsp.util._get_offset_encoding(buf)
+      local start_row = c.range.start.line
+      local start_col = vim.lsp.util._get_line_byte_from_position(buf, c.range.start, offset_encoding)
+      local end_row = c.range["end"].line
+      local end_col = vim.lsp.util._get_line_byte_from_position(buf, c.range["end"], offset_encoding)
+
+      vim.api.nvim_buf_set_extmark(buf, M.ns, start_row, start_col, {
+        end_row = end_row,
+        end_col = end_col,
+        hl_group = M.get_hl(hex),
+        priority = 5000,
+      })
+    end
+  end)
+end
+
+M.update()
