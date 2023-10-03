644a1e3acf95edd061992b307600bcfa5a1e592e
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-03 20:43:54 +0200

    feat: upstreamed statuscolumn and folding to LazyVim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,25 +1,5 @@
 vim.g.mapleader = " "
 
-vim.opt.fillchars = {
-  foldopen = "",
-  foldclose = "",
-  -- fold = "⸱",
-  fold = " ",
-  foldsep = " ",
-  diff = "╱",
-  eob = " ",
-}
-vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
-vim.opt.foldcolumn = "0"
--- vim.opt.foldmethod = "indent"
-vim.opt.foldmethod = "expr"
-if vim.fn.has("nvim-0.10") == 1 then
-  vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-  vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
-end
-
 if vim.fn.has("nvim-0.8") == 1 then
   vim.opt.backup = true
   vim.opt.cmdheight = 0
@@ -36,8 +16,6 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
-
-require("util.status")
 
 -- make all keymaps silent by default
 local keymap_set = vim.keymap.set
