1a360521c0574fe3bc2355fc562afaf6b0795de1
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-08 14:44:42 +0200

    feat(util): pager

--- nvim/lua/util/init.lua
+++ nvim/lua/util/init.lua
@@ -158,13 +158,30 @@ function M.cowboy()
 end
 
 function M.colorize()
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_set_current_buf(buf)
-  local channel = vim.api.nvim_open_term(buf, {})
-  for _, line in ipairs(lines) do
-    vim.api.nvim_chan_send(channel, line .. "\r\n")
+  vim.wo.number = false
+  vim.wo.relativenumber = false
+  vim.wo.statuscolumn = ""
+  vim.wo.signcolumn = "no"
+  vim.opt.listchars = { space = " " }
+  local orig_buf = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(orig_buf, 0, -1, false)
+  while #lines > 0 and vim.trim(lines[#lines]) == "" do
+    lines[#lines] = nil
   end
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.b[buf].minianimate_disable = true
+  local channel = vim.api.nvim_open_term(buf, {})
+  vim.api.nvim_chan_send(channel, table.concat(lines, "\r\n"))
+  vim.api.nvim_set_current_buf(buf)
+  vim.keymap.set("n", "q", "<cmd>qa!<cr>", { silent = true, buffer = buf })
+  vim.api.nvim_create_autocmd("TermEnter", { buffer = buf, command = "stopinsert" })
+  vim.defer_fn(function()
+    -- go to the end of the terminal buffer
+    vim.cmd.startinsert()
+    vim.defer_fn(function()
+      vim.b[buf].minianimate_disable = false
+    end, 200)
+  end, 10)
 end
 
 return M
