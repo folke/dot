edb22b6a34f2964ab6f928b55d7f08d6a4e2a981
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-09 16:35:06 +0200

    feat(fish): clipboard integration for vim bindings

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,9 @@
+
 function fish_user_key_bindings
     set -g fish_key_bindings fish_vi_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
+    bind -M visual -m default y 'fish_clipboard_copy; commandline -f end-selection repaint-mode'
+    bind yy fish_clipboard_copy
+    bind p fish_clipboard_paste
 end
