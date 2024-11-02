9faae983640f9775eeb1eaa0192346532a779156
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-02 20:07:18 +0100

    style(nvim): cleanup init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,35 +6,6 @@ if vim.loader then
   vim.loader.enable()
 end
 
--- local cache_root = vim.fn.stdpath("cache") .. "/fast"
--- local _, cache = pcall(function()
---   return loadfile(cache_root .. "/cache.lua")()
--- end)
--- cache = cache or {}
--- table.insert(package.loaders, 2, function(mod)
---   local file = cache[mod]
---   if file then
---     local chunk, err = loadfile(file)
---     return chunk, err
---   else
---     -- vim.schedule(function()
---     --   dd(mod)
---     -- end)
---   end
--- end)
---
--- vim.api.nvim_create_autocmd("VimLeavePre", {
---   callback = function(ev)
---     vim.fn.mkdir(cache_root, "p")
---     cache = {}
---     for modname in pairs(package.loaded) do
---       local info = vim.loader.find(modname)[1]
---       cache[modname] = info and info.modpath or nil
---     end
---     require("lazy.util").write_file(cache_root .. "/cache.lua", "return " .. vim.inspect(cache))
---   end,
--- })
-
 _G.dd = function(...)
   require("util.debug").dump(...)
 end
