0f05442907c8c097c2d0419574775031c0908821
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-28 09:58:12 +0100

    chore(nvim): disable perfanno

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -32,19 +32,19 @@ return {
       },
     },
   },
-  {
-    "t-troebst/perfanno.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    opts = function()
-      local util = require("perfanno.util")
-      local hl = vim.api.nvim_get_hl(0, { name = "Normal" })
-      local bg = string.format("#%06x", hl.bg)
-      local fg = "#dc2626"
-      return {
-        line_highlights = util.make_bg_highlights(bg, fg, 10),
-        vt_highlight = util.make_fg_highlight(fg),
-      }
-    end,
-    cmd = "PerfLuaProfileStart",
-  },
+  -- {
+  --   "t-troebst/perfanno.nvim",
+  --   dependencies = { "nvim-telescope/telescope.nvim" },
+  --   opts = function()
+  --     local util = require("perfanno.util")
+  --     local hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+  --     local bg = string.format("#%06x", hl.bg)
+  --     local fg = "#dc2626"
+  --     return {
+  --       line_highlights = util.make_bg_highlights(bg, fg, 10),
+  --       vt_highlight = util.make_fg_highlight(fg),
+  --     }
+  --   end,
+  --   cmd = "PerfLuaProfileStart",
+  -- },
 }
