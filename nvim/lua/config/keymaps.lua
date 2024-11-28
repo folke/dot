91fdca31b9053eabe5df101f2009e1ecf2aed29b
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-28 09:53:02 +0100

    fix(nvim): disable wezterm keymap integration

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,10 @@
 local util = require("util")
 
 util.cowboy()
-util.wezterm()
+-- util.wezterm()
 
--- change word with <c-c>
-vim.keymap.set({ "n", "x" }, "<C-c>", "<cmd>normal! ciw<cr>a")
+vim.keymap.set("n", "<C-c>", "ciw")
+vim.keymap.set("n", "<Up>", "<c-w>k")
+vim.keymap.set("n", "<Down>", "<c-w>j")
+vim.keymap.set("n", "<Left>", "<c-w>h")
+vim.keymap.set("n", "<Right>", "<c-w>l")
