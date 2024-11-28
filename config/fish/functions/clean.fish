3dfcbe418c230dda6378358dbf45f19c3c1e903b
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-28 09:46:26 +0100

    fix(fish): update clean script

--- config/fish/functions/clean.fish
+++ config/fish/functions/clean.fish
@@ -1,13 +1,13 @@
 function clean
     sudo paccache -rk1
     sudo paccache -ruk0
-    paru -Scca --noconfirm
+    paru -Sccd --noconfirm
     cargo cache -a
     yarn cache clean
     pnpm store prune
     pip cache purge
-    sudo docker system prune -a
-    podman system prune -a
+    sudo docker system prune -a -f
+    podman system prune -a -f
     start snapper-cleanup.service
     sudo systemctl status snapper-cleanup.service
     duf
