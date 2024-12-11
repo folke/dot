20241fd852b5b677af1def4ed525a7de3d487d73
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-11 12:46:41 +0100

    feat(nvim): latest snacks stuff

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,50 +2,72 @@ return {
   { "yanky.nvim", keys = { { "<leader>p", false, mode = { "n", "x" } } } },
   {
     "snacks.nvim",
+    ---@type snacks.Config
     opts = {
-      profiler = { runtime = "~/projects/neovim/runtime/" },
+      profiler = {
+        runtime = "~/projects/neovim/runtime/",
+        presets = {
+          on_stop = function()
+            Snacks.profiler.scratch()
+          end,
+        },
+      },
+      input = {},
+      -- styles = { terminal = { position = "top" } },
+      indent = {
+        scope = {
+          treesitter = {
+            -- blocks = true,
+            enabled = true,
+          },
+        },
+      },
+      ---@type snacks.scroll.Config
+      scroll = {},
       dashboard = { example = "github" },
+      -- dashboard = { example = "advanced" },
       gitbrowse = {
-        config = function(opts, defaults)
-          dd(opts, defaults)
-        end,
         open = function(url)
           vim.fn.system(" ~/dot/config/hypr/scripts/quake")
           vim.ui.open(url)
         end,
       },
     },
+    -- stylua: ignore
     keys = {
-      {
-        "<leader>pp",
-        function()
-          if not Snacks.profiler.toggle() then
-            Snacks.profiler.pick({ min_time = 0.2 })
-          end
-        end,
-      },
-      {
-        "<leader>ph",
-        function()
-          Snacks.profiler.highlight()
-        end,
-      },
+      -- { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+      { "<leader>Z", function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+      { "<leader>ps", function() Snacks.profiler.scratch() end },
       {
         "<leader>pd",
         function()
-          if not Snacks.profiler.enabled then
+          if not Snacks.profiler.running() then
             Snacks.notify("Profiler debug started")
             Snacks.profiler.start()
           else
             Snacks.profiler.debug()
             Snacks.notify("Profiler debug stopped")
           end
-          -- Snacks.profiler.toggle()
-          if not Snacks.profiler.enabled then
-            Snacks.profiler.pick({})
-          end
         end,
       },
     },
+  },
+  {
+    "snacks.nvim",
+    opts = function()
+      LazyVim.on_load("which-key.nvim", function()
+        Snacks.toggle.profiler():map("<leader>pp")
+        Snacks.toggle.profiler_highlights():map("<leader>ph")
+        Snacks.toggle.indent():map("<leader>ug")
+        Snacks.toggle.dim():map("<leader>uD")
+        Snacks.toggle.zen():map("<leader>z")
+      end)
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, Snacks.profiler.status())
+    end,
   },
 }
--- nvim/lua/plugins/tmp.lua
+++ nvim/lua/plugins/tmp.lua
@@ -1,3 +1,27 @@
 return {
+  { "mini.animate", enabled = false },
+  { "indent-blankline.nvim", enabled = false },
+
+  -- {
+  --   "foobar",
+  --   event = "VeryLazy",
+  --   virtual = true,
+  --   config = function()
+  --     vim.notify("foo")
+  --   end,
+  -- },
+  -- { "sindrets/diffview.nvim", opts = {}, lazy = false },
+  -- { "folke/zen-mode.nvim", opts = {}, lazy = false },
+  -- { "conform.nvim", dev = true },
   -- { "smear-cursor.nvim", dev = true },
+  -- { "blink.cmp", dev = true },
+  {
+    "sphamba/smear-cursor.nvim",
+    enabled = false,
+    cond = not (vim.env.KITTY_PID or vim.g.neovide),
+  },
+  -- {
+  --   "mini.animate",
+  --   opts = { cursor = { enable = not (vim.env.KITTY_PID or vim.g.neovide) } },
+  -- },
 }
