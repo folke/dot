6cc9b9bc3eaaf7191f32a14d30057381ae5cfcbe
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-09-24 14:38:43 +0200

    feat(pkgdiff): diff files with sudoedit

--- config/fish/functions/pkgdiff.fish
+++ config/fish/functions/pkgdiff.fish
@@ -16,17 +16,17 @@ function pkgdiff -a file
     # latest cached pkg archive
     set cache (command ls -t /var/cache/pacman/pkg/$pkg-*.pkg.tar.zst | head -1)
     set rel (string replace -r '^/' '' -- $file)
+    echo "Comparing $file with $pkg ($cache)"
 
     # extract packaged version to a temp file
-    set pkg_tmp (mktemp)
+    set pkg_tmp (mktemp /tmp/original.XXX.(basename $file))
     if not bsdtar -xOf "$cache" "$rel" >"$pkg_tmp" 2>/dev/null
         echo "Not found in package: $rel"
         rm -f "$pkg_tmp"
         return 1
     end
 
-    nvim -d "$file" "$pkg_tmp"
+    SUDO_EDITOR="$(which nvim) -d \"$pkg_tmp\"" sudoedit "$file"
 
-    # cleanup
     rm -f "$pkg_tmp"
 end
