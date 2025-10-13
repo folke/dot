e782976ce12efb6912211d543074b6bbe4e48231
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-13 13:17:46 +0200

    chore(nvim): remove peek.nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -53,20 +53,4 @@ return {
   },
 
   { "markdown-preview.nvim", enabled = false },
-
-  {
-    "toppair/peek.nvim",
-    build = "deno task --quiet build:fast",
-    opts = {
-      theme = "light",
-    },
-    keys = {
-      {
-        "<leader>cp",
-        function()
-          require("peek").open()
-        end,
-      },
-    },
-  },
 }
